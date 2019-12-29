d8ddde50680f96ceca715dc026663c58490b4d6d
orizitzer365 <orizit365@gmail.com>
2019-12-29 19:59:01 +0200

    update

--- Fair&Efficient_Cake_Division_with_Connected_Pieces.py
+++ Fair&Efficient_Cake_Division_with_Connected_Pieces.py
@@ -8,13 +8,22 @@ Date : 27/12/19
 from agents import *
 from allocations import *
 
-def ALG(agents: List[Agent])->Allocation:
+def ALG(agents: List[Agent],epsilon)->Allocation:
     """
         ALG: Algorithm that find Fair and Efficient Cake Division with Connected Pieces
 
         :param agents: a list that must contain at least 2 Agent objects.
+                epsilon: constant between 0 to 1/3
         :return: a Fair and Efficient allocation.
+        >>> Alice = PiecewiseConstantAgent([33,33], "Alice")
+        >>> ALG([Alice])
+        > Alice gets [(0, 1)] with value 66.00
+        <BLANKLINE>
     """
+    allocation = Allocation(agents)
+    while():
+
+
 
 if __name__ == "__main__":
     import doctest
