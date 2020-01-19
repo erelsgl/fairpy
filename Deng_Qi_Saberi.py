e24c7914f88de8f099e3a5edac9448df0e410dc3
Fried Dvir <friedvir1@gmail.com>
2020-01-19 22:27:07 +0200

    starting to work
    adding the file, trying to figure a possible name, and looking for what needed exactly to be done

---
+++ Deng_Qi_Saberi.py
@@ -0,0 +1,16 @@
+#!python3
+
+"""
+Truthful auction of Heterogeneous Divisible Goods with an approximation algorithm.
+References:
+    Xiaotie Deng, Qi Qi, Amin Saberi (2012):
+    "Algorithmic Solutions for Envy-Free Cake Cutting"
+    Algorithm 1.
+Programmer: Dvir Fried
+Since: 2020-01
+"""
+
+# naming for function: Algorithm 1. this function is about being recursive one.
+
+# naming for function inside: index(V(i_1, i_2, k_1, k_2).
+# this function is the one who divides V to subsets under those boundaries.
