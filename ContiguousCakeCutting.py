e7619bdd00fb1d5ab59daf6244295a4f82776aeb
shalev61 <46303113+shalev61@users.noreply.github.com>
2020-04-05 12:55:27 +0300

    fixing agents

--- ContiguousCakeCutting.py
+++ ContiguousCakeCutting.py
@@ -14,209 +14,6 @@ from typing import *
 import logging
 logger = logging.getLogger(__name__)
 
-class Agent(ABC):
-    """
-    An abstract class that describes a participant in a cake-cutting algorithm.
-    It can answer the standard "mark" and "eval" queries.
-    It may also have a name, which is used only in demonstrations and for tracing. The name may be left blank (None).
-    """
-
-    def __init__(self, name: str = None):
-        if name is not None:
-            self.my_name = name
-
-    def name(self):
-        if hasattr(self, 'my_name') and self.my_name is not None:
-            return self.my_name
-        else:
-            return "Anonymous"
-
-    @abstractmethod
-    def cake_value(self):
-        """
-        :return: the value of the entire cake for the agent.
-        """
-        pass
-
-    @abstractmethod
-    def cake_length(self):
-        """
-        :return: the total length of the cake that the agent cares about.
-        """
-        pass
-
-    @abstractmethod
-    def eval(self, start: float, end: float):
-        """
-        Answer an Eval query: return the value of the interval [start,end].
-        :param start: Location on cake where the calculation starts.
-        :param end:   Location on cake where the calculation ends.
-        :return: Value of [start,end]
-        """
-        pass
-
-    @abstractmethod
-    def mark(self, start: float, targetValue: float):
-        """
-        Answer a Mark query: return "end" such that the value of the interval [start,end] is targetValue.
-        :param start: Location on cake where the calculation starts.
-        :param targetValue: required value for the piece [start,end]
-        :return: the end of an interval with a value of targetValue.
-        """
-        pass
-
-    def piece_value(self, piece: List[tuple]):
-        """
-        Evaluate a piece made of several intervals.
-        :param piece: a list of tuples [(start1,end1), (start2,end2),...]
-        :return:
-        """
-        if (piece == None):
-            return 0
-        return sum([self.eval(*interval) for interval in piece])
-
-    def partition_values(self, partition: List[float]):
-        """
-        Evaluate all the pieces in the given partition.
-        :param partition: a list of k cut-points [cut1,cut2,...]
-        :return: a list of k+1 values: eval(0,cut1), eval(cut1,cut2), ...
-        >>> a = PiecewiseConstantAgent([1,2,3,4])
-        >>> a.partition_values([1,2])
-        [0.1, 0.2, 0.7]
-        >>> a.partition_values([3,3])
-        [0.6, 0.0, 0.4]
-        """
-
-        values = []
-        values.append(self.eval(0, partition[0]))
-        for i in range(len(partition) - 1):
-            values.append(self.eval(partition[i], partition[i + 1]))
-        values.append(self.eval(partition[-1], self.cake_length()))
-        return values
-
-
-class PiecewiseConstantAgent(Agent):
-
-
-    # the init of the agent
-    def __init__(self, values: list, name: str = None):
-        super().__init__(name)
-
-        # the len of the values list
-        self.length = len(values)
-        self.total_value_cache = sum(values)
-        # self.values = np.array(values)
-        ls = [None] * len(values)
-        # normalize the cake to values between 0.0 and 1.0
-        # by dividing the values by there sum
-        for i in range(len(values)):
-            ls[i] = values[i] / self.total_value_cache
-
-        # the list of the values
-        self.values = np.array(ls)
-
-        self.normal = 1 / self.total_value_cache
-
-    def __repr__(self):
-        return "{} is a piecewise-constant agent with values {} (((NORMALIZED)))".format(self.my_name, self.values)
-
-    def cake_value(self):
-        return self.total_value_cache
-
-    def cake_length(self):
-        return self.length
-
-    # the evaluation of the agents
-    def eval(self, start: float, end: float):
-        """
-        Answer an Eval query: return the value of the interval [start,end] {{{[(NORMALIZED)]}}}.
-
-        :param start: Location on cake where the calculation starts.
-        :param end:   Location on cake where the calculation ends.
-        :return: Value of [start,end]
-
-        >>> a = PiecewiseConstantAgent([11,22,33,44])
-        >>> a.eval(0.5,1)
-        0.7
-        >>> a.eval(0.25,1)
-        0.9
-        >>> a.eval(0,0.25)
-        0.1
-        >>> a.eval(0,0.375)
-        0.2
-
-        """
-        # the cake to the left of 0 and to the right of length is considered worthless.
-        start = max(0, min(start * self.length, self.length))
-        end = max(0, min(end * self.length, self.length))
-        if end <= start:
-            return 0.0  # special case not covered by loop below
-
-        fromFloor = int(np.floor(start))
-        fromFraction = (fromFloor + 1 - start)
-        toCeiling = int(np.ceil(end))
-        toCeilingRemovedFraction = (toCeiling - end)
-
-        val = 0.0
-        val += (self.values[fromFloor] * fromFraction)
-        val += self.values[fromFloor + 1:toCeiling].sum()
-        val -= (self.values[toCeiling - 1] * toCeilingRemovedFraction)
-
-        return val
-
-    # the mark is for finding the point where it's equals to  the target value
-    def mark(self, start: float, target_value: float):
-        """
-        Answer a Mark query: return "end" such that the value of the interval [start,end] is target_value  {[(NORMALIZED)]}.
-
-        :param start: Location on cake where the calculation starts.
-        :param targetValue: required value for the piece [start,end]
-        :return: the end of an interval with a value of target_value.
-        If the value is too high - returns None.
-
-        >>> a = PiecewiseConstantAgent([11,22,33,44])
-        >>> a.mark(0.5, 0.7)
-        1
-        >>> a.mark(0.375, 0.1)
-        0.5
-        >>> a.mark(0, 0.2)
-        0.375
-        >>> a.mark(0.25, 0.2)
-        0.5
-        >>> a.mark(0, 0.9)
-        0.9375
-
-        """
-        # the cake to the left of 0 and to the right of length is considered worthless.
-        start = max(0, start * self.length)
-        if start >= self.length:
-            return None  # value is too high
-
-        if target_value < 0:
-            raise ValueError("sum out of range (should be positive): {}".format(sum))
-
-        start_floor = int(np.floor(start))
-        if start_floor >= len(self.values):
-            raise ValueError(
-                "mark({},{}): start_floor ({}) is >= length of values ({})".format(start, target_value, start_floor,
-                                                                                   self.values))
-
-        start_fraction = (start_floor + 1 - start)
-
-        value = self.values[start_floor]
-        if value * start_fraction >= target_value:
-            # i added the division by self.length for normalzing
-            return ((start + (target_value / value)) / self.length)
-        target_value -= (value * start_fraction)
-        for i in range(start_floor + 1, self.length):
-            value = self.values[i]
-            if target_value <= value:
-                # i added the division by self.length for normalzing
-                return ((i + (target_value / value)) / self.length)
-            target_value -= value
-
-        # Value is too high: return None
-        return None
 
 # line 5. 'some agent in N values [`, 1] at least 1/3'
 # agentN - indexes of all existing agents
@@ -231,8 +28,8 @@ def hasBiggerThanThird(left, agentN, agentL):
     :return: true if there is at list one the has value more then 1/3 .
     & false where there is no one that has value that is more or equal to 1/3.
 
-    >>> c1 = PiecewiseConstantAgent([2, 8, 2])
-    >>> c2 = PiecewiseConstantAgent([4, 2, 6])
+    >>> c1 = PiecewiseConstantAgentNormalized([2, 8, 2])
+    >>> c2 = PiecewiseConstantAgentNormalized([4, 2, 6])
     >>> lsC = [c1,c2]
     >>> hasBiggerThanThird(0,2,lsC)
     True
@@ -263,16 +60,16 @@ def algor1(AgentList):
     :return: the end of an interval with a value of target_value.
     If the value is too high - returns None.
 
-    >>> aa = ContiguousCakeCutting.PiecewiseConstantAgent([2, 8, 2], name="aa")
-    >>> bb = ContiguousCakeCutting.PiecewiseConstantAgent([4, 2, 6], name="bb")
+    >>> aa = PiecewiseConstantAgentNormalized([2, 8, 2], name="aa")
+    >>> bb = PiecewiseConstantAgentNormalized([4, 2, 6], name="bb")
     >>> lstba = [aa,bb]
     >>> algor1(lstba)
     > Agent aa gets the segment: [0.3333333333333333, 1.0]
     > Agent bb gets the segment: [0.0, 0.3333333333333333]
 
-    >>> a0 = ContiguousCakeCutting.PiecewiseConstantAgent([4, 10, 20], name="a0")
-    >>> a1 = ContiguousCakeCutting.PiecewiseConstantAgent([14, 42, 9, 17], name="a1")
-    >>> a2 = ContiguousCakeCutting.PiecewiseConstantAgent([30, 1, 12], name="a2")
+    >>> a0 = PiecewiseConstantAgentNormalized([4, 10, 20], name="a0")
+    >>> a1 = PiecewiseConstantAgentNormalized([14, 42, 9, 17], name="a1")
+    >>> a2 = PiecewiseConstantAgentNormalized([30, 1, 12], name="a2")
     >>> lstaaa = [a0,a1,a2]
     >>> algor1(lstaaa)
     > Agent a0 gets the segment: [0.3824514991181658, 1.0]
--- ContiguousCakeCutting_demo.py
+++ ContiguousCakeCutting_demo.py
@@ -17,8 +17,8 @@ ContiguousCakeCutting.logger.setLevel(logging.INFO)
 
 
 print("\n### Example 1: \n")
-Erel = ContiguousCakeCutting.PiecewiseConstantAgent([2, 8, 2], name="Erel")
-Shalev = ContiguousCakeCutting.PiecewiseConstantAgent([4, 2, 6], name="Shalev")
+Erel = PiecewiseConstantAgentNormalized([2, 8, 2], name="Erel")
+Shalev = PiecewiseConstantAgentNormalized([4, 2, 6], name="Shalev")
 lstES = [Erel,Shalev]
 print(Erel)
 print(Shalev)
@@ -32,10 +32,10 @@ print()
 
 
 print("\n### Example 2: \n")
-ALICE = ContiguousCakeCutting.PiecewiseConstantAgent([11, 22, 33, 44], "ALICE")
-BOB = ContiguousCakeCutting.PiecewiseConstantAgent([7, 13, 51, 12], "BOB")
-CHARLIE = ContiguousCakeCutting.PiecewiseConstantAgent([60, 31, 18, 3], "CHARLIE")
-DANY = ContiguousCakeCutting.PiecewiseConstantAgent([4, 10, 43, 63], "DANY")
+ALICE = PiecewiseConstantAgentNormalized([11, 22, 33, 44], "ALICE")
+BOB = PiecewiseConstantAgentNormalized([7, 13, 51, 12], "BOB")
+CHARLIE = PiecewiseConstantAgentNormalized([60, 31, 18, 3], "CHARLIE")
+DANY = PiecewiseConstantAgentNormalized([4, 10, 43, 63], "DANY")
 lstABCD = [ALICE, BOB, CHARLIE, DANY]
 print(ALICE)
 print(BOB)
@@ -66,9 +66,9 @@ ContiguousCakeCutting.algor1(lstABCD)
 print()
 print("\n### Example 3: \n")
 
-BEN = ContiguousCakeCutting.PiecewiseConstantAgent([4, 10, 20], name="BEN")
-GUR = ContiguousCakeCutting.PiecewiseConstantAgent([14, 42, 9, 17], name="GUR")
-ION = ContiguousCakeCutting.PiecewiseConstantAgent([30, 1, 12], name="ION")
+BEN = PiecewiseConstantAgentNormalized([4, 10, 20], name="BEN")
+GUR = PiecewiseConstantAgentNormalized([14, 42, 9, 17], name="GUR")
+ION = PiecewiseConstantAgentNormalized([30, 1, 12], name="ION")
 lstaaa = [BEN,GUR,ION]
 
 print(BEN)
