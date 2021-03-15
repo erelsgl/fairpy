4228a7f5d02982042130bbcc6d5ce94cff81fa38
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-03-15 19:43:04 +0200

    remove old files; min sharing demo

--- divisible/min_sharing_demo.py
+++ divisible/min_sharing_demo.py
@@ -8,21 +8,21 @@ Since:  2021-03
 """
 
 import numpy as np, cvxpy
-from fairpy.divisible.max_product import *
+from fairpy.divisible.min_sharing import *
 from fairpy.divisible.ValuationMatrix import ValuationMatrix
 from fairpy.divisible.AllocationMatrix import AllocationMatrix
 
 print("\nThree identical goods:")
 v = ValuationMatrix([ [3,3,3] , [2,2,2] , [1,1,1] ])
 print("v = \n",v)
-z = max_product_allocation(v)
-print("z = \n",z, "product = ", product_of_utilities(z, v))
+z = proportional_allocation_with_min_sharing(v)
+print("z = \n",z)
 
 
 print("\nThree different goods:")
 v = ValuationMatrix([ [3,2,2] , [1,2,1] , [0,0,1] ])
 print("v = \n",v)
-z = max_product_allocation(v)
-print("z = \n",z, "product = ", product_of_utilities(z, v))
+z = proportional_allocation_with_min_sharing(v)
+print("z = \n",z)
 
 
