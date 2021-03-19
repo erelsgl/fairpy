c3d7e5c5d92d0961d25a39cf67aa28f84049c132
Erel Segal-Halevi <erelsgl@gmail.com>
2021-03-19 09:47:46 +0200

    fix dependencies

--- divisible/min_sharing_impl/FairAllocationProblem.py
+++ divisible/min_sharing_impl/FairAllocationProblem.py
@@ -12,9 +12,8 @@ from fairpy.divisible.allocations import AllocationMatrix
 
 from fairpy.divisible.min_sharing_impl.ConsumptionGraph import ConsumptionGraph
 from fairpy.divisible.min_sharing_impl.GraphGenerator import GraphGenerator
-from fairpy.divisible.max_product import product_of_utilities
 
-from time_limit import time_limit, TimeoutException
+from fairpy.divisible.min_sharing_impl.time_limit import time_limit, TimeoutException
 
 from abc import ABC, abstractmethod
 import datetime, cvxpy
@@ -52,7 +51,22 @@ class FairAllocationProblem():
         pass
 
 
-    def find_allocation_with_min_sharing(self, min_sharing_allocation:int=3)->AllocationMatrix:
+    def find_allocation_for_all_graphs_with_numsharings(self, allowed_num_of_sharings:int)->AllocationMatrix:
+        allocation = None
+        logger.info("Looking for %s allocations with %d sharings", self.fairness_adjective(), allowed_num_of_sharings)
+        self.graph_generator.set_maximum_allowed_num_of_sharings(allowed_num_of_sharings)
+        for consumption_graph in self.graph_generator.generate_all_consumption_graph():
+            if consumption_graph.get_num_of_sharing() != allowed_num_of_sharings:
+                continue
+            allocation = self.find_allocation_for_graph(consumption_graph)
+            if allocation is not None:
+                logger.info(" -- Found an allocation with %d sharings, for consumption graph: \n%s", allowed_num_of_sharings, consumption_graph)
+                logger.debug("-- Unrounded allocation:\n%s", allocation)
+                break
+        return allocation
+
+
+    def find_allocation_with_min_sharing(self, num_of_decimal_digits:int=3)->AllocationMatrix:
         """
         Runs the min-sharing algorithm on this valuation matrix.
 
@@ -60,28 +74,18 @@ class FairAllocationProblem():
         """
         allowed_num_of_sharings = 0
         logger.info("")
+        allocation = None
         while (allowed_num_of_sharings < self.valuation.num_of_agents) and (not self.find):
-            logger.info("Looking for %s allocations with %d sharings", self.fairness_adjective(), allowed_num_of_sharings)
-            self.graph_generator.set_maximum_allowed_num_of_sharings(allowed_num_of_sharings)
-            for consumption_graph in self.graph_generator.generate_all_consumption_graph():
-                if consumption_graph.get_num_of_sharing() != allowed_num_of_sharings:
-                    continue
-                alloc = self.find_allocation_for_graph(consumption_graph)
-                if alloc is None:
-                    continue
-                self.find = True
-                self.min_sharing_allocation = alloc
-                logger.info(" -- Found an allocation with %d sharings, for consumption graph: \n%s", allowed_num_of_sharings, consumption_graph)
-                logger.debug("-- Unrounded allocation:\n%s", alloc)
-                break
-            if self.find:
+            allocation = self.find_allocation_for_all_graphs_with_numsharings(allowed_num_of_sharings)
+            if allocation is not None:
                 break
             allowed_num_of_sharings += 1
-        alloc.round(min_sharing_allocation)
-        self.min_sharing_number = alloc.num_of_sharings()
-        if self.min_sharing_number >= self.valuation.num_of_agents:
-            raise AssertionError("Num of sharings is {} but it should be at most {}.\n{}".format(self.min_sharing_number, self.valuation.num_of_agents-1,alloc))
-        return self.min_sharing_allocation
+        if allocation is None:
+            raise AssertionError("No allocation found")
+        allocation.round(num_of_decimal_digits)
+        if allocation.num_of_sharings() >= self.valuation.num_of_agents:
+            raise AssertionError("Num of sharings is {} but it should be at most {}.\n{}".format(self.min_sharing_number, self.valuation.num_of_agents-1,allocation))
+        return allocation
 
 
     def find_min_sharing_allocation_with_time_limit(self, num_of_decimal_digits:int=3, time_limit_in_seconds=999)->(str,float,AllocationMatrix,float):
@@ -95,26 +99,21 @@ class FairAllocationProblem():
             with time_limit(time_limit_in_seconds):
                 allocation_matrix = self.find_allocation_with_min_sharing()
                 status = "OK" if allocation_matrix.num_of_sharings() < self.valuation.num_of_agents else "Bug"
-                prod_of_utils = product_of_utilities(allocation_matrix, self.valuation)
         except TimeoutException:
             status = "TimeOut"
-            prod_of_utils =  -1
             allocation_matrix = ErrorAllocationMatrix()
         except cvxpy.error.SolverError:
             status = "SolverError"
-            prod_of_utils = -1
             allocation_matrix = ErrorAllocationMatrix()
         except SystemError:
             status = "SystemError"
-            prod_of_utils =  -1
             allocation_matrix = ErrorAllocationMatrix()
         except AssertionError:  # Indicates too many sharings
             status = "Bug"
-            prod_of_utils =  -1
             allocation_matrix = ErrorAllocationMatrix()
         end = datetime.datetime.now()
         time_in_seconds = (end - start).total_seconds()
-        return (status, time_in_seconds, allocation_matrix, prod_of_utils)
+        return (status, time_in_seconds, allocation_matrix)
 
 
 
@@ -128,3 +127,11 @@ class ErrorAllocationMatrix(AllocationMatrix):
 
     def num_of_sharings(self): 
         return -1
+
+    def utility_profile(self, v):
+        return [0]
+
+    def __repr__(self):
+        return "ErrorAllocationMatrix"
+
+
