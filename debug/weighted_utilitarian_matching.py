6c499068371198e40c1299837d6102403bcf3ff2
Erel Segal-Halevi cron <erelsgl@gmail.com>
2021-05-19 09:19:53 +0300

    iterated maximum matching by categories

--- debug/weighted_utilitarian_matching.py
+++ debug/weighted_utilitarian_matching.py
@@ -16,71 +16,7 @@ IDEA: find a utilitarian (=maximum weight) matching in each category. This guara
 * Is there a vector of weights for which the utilitarian matching is EF1?
 """
 
-from fairpy.items.utilitarian_matching import utilitarian_matching
-from fairpy.allocations import Allocation
-from typing import List,Any,Dict
-
-def weighted_utilitarian_matching(agents:Dict[str,Dict[Any,float]], categories: List[List[Any]], agent_weights:Dict[str,float]):
-	"""
-	:param categories: a list of categories; each one contains a list of items.
-	:param agents: maps an agent name to the agent's valuation. Each valuation is a map from item name to its value.
-	:param agent_weights: maps an agent name to the agent's weight-factor.
-
-	:return a matching in which each agent gets a single item from each category. 
-	        It is a maximum-weight matching in each category, with each agent's weights multiplied by the agent's factor.
-	"""
-	agent_names = sorted(agents.keys())
-	map_agent_to_final_bundle = {name:[] for name in agent_names}
-	for index,category in enumerate(categories):
-		alloc = utilitarian_matching(agents, item_capacities={item:1 for item in category}, agent_weights=agent_weights)
-		map_agent_to_bundle = alloc.map_agent_to_bundle()
-		print(f"Category {index}: {map_agent_to_bundle}")
-		for name in agent_names:
-			if map_agent_to_bundle[name] is not None:
-				map_agent_to_final_bundle[name] += map_agent_to_bundle[name]
-
-	return Allocation(agents, map_agent_to_final_bundle)
-
-
-"""
-Agents are named A, B, C, ...
-Categories are named 1, 2, ...
-Items are named 1x, 1y, ... 2x, 2y, ...
-"""
-
-categories = [
-	["0x","0y","0z"],
-	["1x","1y","1z"],
-	["2x","2y","2z"],
-]
-
-agents2 = {
-	"a": {"0x": -1, "0y": -10,                   "1x": -1, "1y": -11,      "2x": -1, "2y": -15},
-	"b": {"0x": -2, "0y": -25,                   "1x": -3, "1y": -32,      "2x": -4, "2y": -25},
-	# "b": {"0x": 1, "0y": 12.5,              	 "1x": 1, "1y": 10.7},
-	# "b": {"0x": 2*11/27, "0y": 25*11/27,       "1x": 3*12/35, "1y": 32*12/35},
-}
-
-# Example showing that using a different weight in each category might lead to envy cycles.
-agents2 = {
-	"a": {"0x": -1, "0y": -10,                   "1x": -4, "1y": -5},
-	"b": {"0x": -2, "0y": -3,                    "1x": -1, "1y": -12},
-}
-
-agent_weights2 = {"a": 1, "b": 1}
-
-# allocation = weighted_utilitarian_matching(agents2, categories, agent_weights2)
-
-
-
-# agents3 = {
-# 	"a": {"0x": 2, "0y": 3, "0z": 4,         "1x": 4, "1y": 3, "1z": 2},
-# 	"b": {"0x": 20, "0y": 25, "0z": 30,      "1x": 30, "1y": 25, "1z": 20},
-# 	"c": {"0x": 200, "0y": 205, "0z": 210,   "1x": 210, "1y": 205, "1z": 200},
-# }
-
-# allocation = weighted_utilitarian_matching(agents3, categories, agent_weights3)
-
+from fairpy.items.iterated_maximum_matching import iterated_maximum_matching_categories
 
 agents = {
 	"agent1": {"t11": 0, "t12": -3,   "t23": 0, "t24": -9,   "t35": 0, "t36": -2},
@@ -91,8 +27,8 @@ categories = [
 	["t23","t24"],
 	["t35","t36"],
 ]
-weight1 = 1/2-0.01
+weight1 = 1
 agent_weights = {"agent1": weight1, "agent2": 1-weight1}
-allocation = weighted_utilitarian_matching(agents, categories, agent_weights=agent_weights)
+allocation = iterated_maximum_matching_categories(agents, categories, agent_weights=agent_weights)
 print(allocation.str_with_value_matrix())
 
--- items/iterated_maximum_matching.py
+++ items/iterated_maximum_matching.py
@@ -41,12 +41,12 @@ def remove_items(item_capacities:Dict[str,int], items_to_remove:List[str])->Dict
         item_capacities[item] -= 1
     return {item:new_capacity for item,new_capacity in item_capacities.items() if new_capacity > 0}
 
-def iterated_maximum_matching(agents: AgentsDict, agent_weights: Dict[str, int]=None, item_capacities: Dict[str,int]=None):
+def iterated_maximum_matching(agents: AgentsDict, item_capacities: Dict[str,int]=None, agent_weights: Dict[str, int]=None):
     """
     Finds a maximum-weight matching with the given preferences, agent_weights and capacities.
     :param agents: maps each agent to a map from an item's name to its value for the agent.
-    :param agent_weights [optional]: maps each agent to an integer priority. The weights of each agent are multiplied by WEIGHT_BASE^priority.
     :param item_capacities [optional]: maps each item to its number of units. Default is 1.
+    :param agent_weights [optional]: maps each agent to an integer priority. The weights of each agent are multiplied by WEIGHT_BASE^priority.
 
     >>> prefs = {"avi": {"x":5, "y":4, "z":3, "w":2}, "beni": {"x":2, "y":3, "z":4, "w":5}}
     >>> alloc = iterated_maximum_matching(prefs, item_capacities={"x":1,"y":1,"z":1,"w":1})
@@ -57,10 +57,10 @@ def iterated_maximum_matching(agents: AgentsDict, agent_weights: Dict[str, int]=
 
     >>> prefs = [[5,4,3,2],[2,3,4,5]]
     >>> alloc = iterated_maximum_matching(prefs)
-    >>> stringify(alloc.map_agent_to_bundle())
-    '{Agent #0:[0, 1], Agent #1:[3, 2]}'
-    >>> stringify(alloc.map_item_to_agents())
-    "{0:['Agent #0'], 1:['Agent #0'], 2:['Agent #1'], 3:['Agent #1']}"
+    >>> alloc
+    Agent #0 gets {0,1} with value 9.
+    Agent #1 gets {2,3} with value 9.
+    <BLANKLINE>
     """
     agents_list = fairpy.agents_from(agents)
     agent_names = fairpy.agent_names_from(agents)
@@ -78,7 +78,59 @@ def iterated_maximum_matching(agents: AgentsDict, agent_weights: Dict[str, int]=
             map_agent_to_final_bundle[agent] += bundle
         allocated_items = sum([bundle for bundle in map_agent_to_bundle.values()], [])
         item_capacities = remove_items(item_capacities, allocated_items)
-    return Allocation(agents, map_agent_to_final_bundle)
+    return Allocation(agents_list, map_agent_to_final_bundle)
+
+
+
+def iterated_maximum_matching_categories(agents: AgentsDict, categories: List[List[str]], agent_weights: Dict[str, int]=None):
+    """
+    Finds a maximum-weight matching with the given preferences and agent_weights, where the items are pre-divided into categories. Each agent gets at most a single item from each category.
+    :param agents: maps each agent to a map from an item's name to its value for the agent.
+    :param categories: a list of lists; each list is a category of items.
+    :param agent_weights [optional]: maps each agent to an integer priority. The weights of each agent are multiplied by WEIGHT_BASE^priority.
+
+    >>> agents = {"agent1": {"t1+": 0, "t1-": -3,   "t2+": 0, "t2-": -9,   "t3+": 0, "t3-": -2},	"agent2": {"t1+": 0, "t1-": -6,   "t2+": 0, "t2-": -9,   "t3+": 0, "t3-": -1}}
+    >>> categories = [["t1+","t1-"],["t2+","t2-"],["t3+","t3-"]]
+    >>> iterated_maximum_matching_categories(agents, categories, agent_weights={"agent1":1,"agent2":0})
+    agent1 gets {t1+,t2+,t3+} with value 0.
+    agent2 gets {t1-,t2-,t3-} with value -16.
+    <BLANKLINE>
+    >>> iterated_maximum_matching_categories(agents, categories, agent_weights={"agent1":1,"agent2":1})
+    agent1 gets {t1-,t2-,t3+} with value -12.
+    agent2 gets {t1+,t2+,t3-} with value -1.
+    <BLANKLINE>
+    >>> iterated_maximum_matching_categories(agents, categories, agent_weights={"agent1":0,"agent2":1})
+    agent1 gets {t1-,t2-,t3-} with value -14.
+    agent2 gets {t1+,t2+,t3+} with value 0.
+    <BLANKLINE>
+
+    >>> agents = [[55,44,33,22],[22,33,44,55]]
+    >>> iterated_maximum_matching_categories(agents, categories= [[0,1],[2,3]])
+    Agent #0 gets {0,2} with value 88.
+    Agent #1 gets {1,3} with value 88.
+    <BLANKLINE>
+    >>> iterated_maximum_matching_categories(agents, categories= [[0,2],[1,3]])
+    Agent #0 gets {0,1} with value 99.
+    Agent #1 gets {2,3} with value 99.
+    <BLANKLINE>
+    """
+    agents_list = fairpy.agents_from(agents)
+    agent_names = fairpy.agent_names_from(agents)
+    map_agent_to_final_bundle = {agent.name(): [] for agent in agents_list}
+    for index,category in enumerate(categories):
+        graph = instance_to_graph(agents, agent_weights=agent_weights, item_capacities={item:1 for item in category})
+        logger.info("Category %d:",index)
+        logger.info("  Graph edges: %s", list(graph.edges.data()))
+        matching = networkx.max_weight_matching(graph, maxcardinality=True)
+        logger.info("  Matching: %s", matching)
+        map_agent_to_bundle = matching_to_allocation(matching, agent_names=agent_names)
+        for name in agent_names:
+            if map_agent_to_bundle[name] is not None:
+                map_agent_to_final_bundle[name] += map_agent_to_bundle[name]
+    return Allocation(agents_list, map_agent_to_final_bundle)
+
+
+iterated_maximum_matching.logger = logger
 
 
 if __name__ == "__main__":
