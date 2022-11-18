79652d584088b3676ba1ad6b55af5e601e62ff97
Erel Segal-Halevi <erelsgl@gmail.com>
2022-11-18 11:45:50 +0200

    fix demo

--- fairpy/items/bag_filling_demo.py
+++ fairpy/items/bag_filling_demo.py
@@ -8,9 +8,10 @@ Programmer: Erel Segal-Halevi
 Since:  2021-04
 """
 
+import fairpy
 from fairpy.items.bag_filling import one_directional_bag_filling
 
 valuations = [[10,20,30,40,50,60],[60,50,40,30,20,10]]
 thresholds = [100, 100]
-print(one_directional_bag_filling(valuations, thresholds))
+print(fairpy.items.divide(one_directional_bag_filling, valuations, thresholds=thresholds))
 
