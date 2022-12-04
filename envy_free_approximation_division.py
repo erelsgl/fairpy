884beb6b75cf5637bc85b217312cdeefdeff4337
noamya-sh <noamyashany@gmail.com>
2022-12-04 18:26:32 +0200

    add examples to doctest

--- envy_free_approximation_division.py
+++ envy_free_approximation_division.py
@@ -17,18 +17,18 @@ def envy_free_approximation(a:Allocation) -> List[List[Any]]:
     ...      [15,25,22,20]]
     >>> a = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v), bundles=AllocationMatrix(a))
-    {"items":[[3],[1],[0],[2]],"payments":[[10],[12],[5],[0]]}
+    {"bundles":[[3],[1],[0],[2]],"payments":[10,12,5,0]}
     >>> v2 = [[0,50,0,0],
     ...       [0,40,0,0],
     ...       [0,30,0,0],
     ...       [0,45,0,0]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v2), bundles=AllocationMatrix(a))
-    {"items":[[1],[0],[2],[3]],"payments":[[50],[0],[0],[0]]}
+    {"bundles":[[1],[0],[2],[3]],"payments":[50,0,0,0]}
     >>> v3 = [[-5,20,10,25],
     ...      [15,-15,-12,-15],
     ...      [-10,12,9,-5],
     ...      [12,20,30,-10]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v3), bundles=AllocationMatrix(a))
-    {"items":[[3],[0],[1],[2]],"payments":[[5],[27],[3],[0]]}
+    {"bundles":[[3],[0],[1],[2]],"payments":[5,27,3,0]}
     """
     pass
--- envy_freeness_and_equitability_with_payments.py
+++ envy_freeness_and_equitability_with_payments.py
@@ -1,4 +1,4 @@
-from fairpy import Allocation
+from fairpy import Allocation, MonotoneValuation
 
 def envy_freeness_and_equitability_with_payments(a:Allocation):
     """
@@ -9,13 +9,23 @@ def envy_freeness_and_equitability_with_payments(a:Allocation):
 
     Programmers: Noamya Shani, Eitan Shankolevski.
 
-    >>> v = [[70,0,0,0,0],
-    ...      [60,0,0,0,0],
-    ...      [40,0,0,0,0],
-    ...      [80,0,0,0,0],
-    ...      [55,0,0,0,0]]
-    >>> a = [[1,0,0,0,0],[0,1,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,0,0,0,1]]
-    >>> envy_freeness_and_equitability_with_payments(Allocation(agents = ValuationMatrix(v), bundles=AllocationMatrix(a))
-
+    >>> agent_dict = {"A":{"x":70},"B":{"x":60},"C":{"x":40},"D":{"x":80},"E":{"x":55}}
+    >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict, bundles={"A":{"x"}})
+    {"bundles":{"D":{"x"}},"payments":[-16,-16,-16,64,-16]}
+    >>> eq_value = {"x":10,"y":5,"z":15}
+    >>> agent_dict2 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
+    >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict2, bundles={"A":{"x"},"B":{"y"},"C":{"z"}})
+    {"bundles":{"A":{"x"},"B":{"y"},"C":{"z"}},"payments":[2.5,-2.5,7.5,-7.5]}
+    >>> a = MonotoneValuation({"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"xw":20,"yz":30,"yw":30,
+    ... "zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"xyzw":50})
+    >>> b = MonotoneValuation({"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"xw":40,"yz":60,"yw":45,
+    ... "zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"xyzw":95})
+    >>> c = MonotoneValuation({"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"xw":65,"yz":25,"yw":35,
+    ... "zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"xyzw":90})
+    >>> d = MonotoneValuation({"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"xw":25,"yz":25,"yw":30,
+    ... "zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"xyzw":50})
+    >>> agent_dict3 = {"A":a,"B":b,"C":c,"D":d}
+    >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict3, bundles={"A":{"x"},"B":{"y"},"C":{"z"},"D":{"w"}})
+     {"bundles":{"B":{"x","y","z"},"C":{"w"}},"payments":[-27.75,62.25,-6.75,-27.75]}
     """
     pass
