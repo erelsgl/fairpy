5aa86b4547e25e97fe2ded5e05627628b5a763cc
Asif Rot <asif..rot12@gmail.com>
2022-12-05 21:44:25 +0200

    updating tests

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fairpy.agentlist import AgentList
 from fairpy.allocations import Allocation
 
 
-def algorithm1(agentsList: AgentList, rent: int, budget: dict) -> (int, Allocation):
+def algorithm1(agentsList: AgentList, rent: int, budget: dict) -> (int, dict):
     """
     "Fair Rent Division on a Budget", by Procaccia, A., Velez, R., & Yu, D. (2018), https://doi.org/10.1609/aaai.v32i1.11465
     The algorithm calculates Maximum-rent envy-free allocation in a fully connected economy, or in simple words,
@@ -21,21 +21,21 @@ def algorithm1(agentsList: AgentList, rent: int, budget: dict) -> (int, Allocati
     TESTS:
     >>> ex1 = AgentList({"Alice":{'1' : 250, '2' : 250, '3' : 500}, "Bob": {'1': 250, '2' : 250, '3' :500}, "Clair": {'1' :250, '2' : 500, '3' : 250}})
     >>> algorithm1(ex1, 1000, {'Alice': 250, 'Bob': 320, 'Clair': 430})
-    (709.99, (Alice gets {1} with rent 70, Bob gets {2} with rent 320, Clair gets {3} with rent 320))
+    (709.99, {"Alice": {1: 70}, "Bob": {2: 320}, "Clair": {3: 320}})
 
     >>> ex2 = AgentList({"Alice":{'1' : 250, '2' : 750}, "Bob": {'1': 250, '2' : 750}})
     >>> algorithm1(ex2, 1000, {'Alice': 600, 'Bob': 500})
-    (918.75, (Alice gets {1} with rent 600, Bob gets {2} with rent 318.75))
+    (918.75, {"Alice": {1: 600}, "Bob": {2: 318.75}})
 
     >>> ex3 = AgentList({"Alice":{'1' : 400, '2' : 600}, "Bob": {'1': 300, '2' : 700}})
     >>> algorithm1(ex3, 1000, {'Alice': 450, 'Bob': 550})
-    (900, (Alice gets {1} with rent 350, Bob gets {2} with rent 550))
+    (900, {"Alice": {1: 350}, "Bob": {2: 550}})
     """
 
     return 0  # Empty implementation
 
 
-def algorithm2(agentsList: AgentList, rent: int, budget: dict) -> (int, Allocation):
+def algorithm2(agentsList: AgentList, rent: int, budget: dict) -> dict:
     """
     "Fair Rent Division on a Budget", by Procaccia, A., Velez, R., & Yu, D. (2018), https://doi.org/10.1609/aaai.v32i1.11465
     The algorithm calculates fair rent division with optimal envy-free allocation subject to budget constraints.
@@ -52,7 +52,7 @@ def algorithm2(agentsList: AgentList, rent: int, budget: dict) -> (int, Allocati
     TESTS:
     >>> ex1 = AgentList({"Alice":{'1' : 250, '2' : 250, '3' : 500}, "Bob": {'1': 250, '2' : 250, '3' :500}, "Clair": {'1' :250, '2' : 500, '3' : 250}})
     >>> algorithm2(ex1, 1000, {'Alice': 250, 'Bob': 320, 'Clair': 430})
-    (920, (Alice gets {1} with rent 130, Bob gets {2} with rent 270, Clair gets {3} with rent 520))
+    {"Alice": {1: 130}, "Bob": {2: 270}, "Clair": {3: 520}}
 
     >>> ex2 = AgentList({"Alice":{'1' : 250, '2' : 250, '3' : 500}, "Bob": {'1': 250, '2' : 250, '3' :500}, "Clair": {'1': 250, '2' : 250, '3' :500}})
     >>> algorithm2(ex2, 1000, {'Alice': 300, 'Bob': 300, 'Clair': 300})
@@ -60,7 +60,7 @@ def algorithm2(agentsList: AgentList, rent: int, budget: dict) -> (int, Allocati
 
     >>> ex3 = AgentList({"Alice":{'1' : 250, '2' : 750}, "Bob": {'1': 250, '2' : 750}})
     >>> algorithm2(ex3, 1000, {'Alice': 600, 'Bob': 500})
-    (918.75, (Alice gets {1} with rent 600, Bob gets {2} with rent 318.75))
+    {"Alice": {1: 600}, "Bob": {2: 318.75}}
     """
 
     return 0  # Empty implementation
--- test.py
+++ test.py
@@ -8,57 +8,51 @@ class TestMain(unittest.TestCase):
         ex1 = AgentList({"Alice": {'1': 250, '2': 250, '3': 500}, "Bob": {'1': 250, '2': 250, '3': 500},
                          "Clair": {'1': 250, '2': 500, '3': 250}})
         self.assertEqual(algorithm1(ex1, 1000, {'Alice': 250, 'Bob': 320, 'Clair': 430}),
-                         (709.99, "(Alice gets {1} with rent 70, Bob gets {2} with rent 320,"
-                                  "Clair gets {3} with rent 320))"))
+                         (709.99, {"Alice": {1: 70}, "Bob": {2: 320}, "Clair": {3: 320}}))
 
         ex2 = AgentList({"Alice": {'1': 250, '2': 750}, "Bob": {'1': 250, '2': 750}})
         self.assertEqual(algorithm1(ex2, 1000, {'Alice': 600, 'Bob': 500}),
-                         (918.75, "(Alice gets {1} with rent 600, Bob gets {2} with rent 318.75)"))
+                         (918.75, {"Alice": {1: 600}, "Bob": {2: 318.75}}))
 
         ex3 = AgentList({"Alice": {'1': 400, '2': 600}, "Bob": {'1': 300, '2': 700}})
         self.assertEqual(algorithm1(ex3, 1000, {'Alice': 450, 'Bob': 550}),
-                         (900, "(Alice gets {1} with rent 350, Bob gets {2} with rent 550)"))
+                         (900, {"Alice": {1: 350}, "Bob": {2: 550}}))
 
         ex4 = AgentList({"Alice": {'1': 250, '2': 300, '3': 450}, "Bob": {'1': 300, '2': 250, '3': 450},
                          "Clair": {'1': 450, '2': 300, '3': 250}})
         self.assertEqual(algorithm1(ex4, 1000, {'Alice': 300, 'Bob': 320, 'Clair': 350}),
-                         (822.25, "(Alice gets {2} with rent 180, Bob gets {3} with rent 300,"
-                                  "Clair gets {3} with rent 342.25))"))
+                         (822.25, {"Alice": {2: 180}, "Bob": {3: 300}, "Clair": {3: 342.25}}))
 
         ex5 = AgentList({"Alice": {'1': 300, '2': 400}, "Bob": {'1': 320, '2': 380}})
         self.assertEqual(algorithm1(ex5, 700, {'Alice': 310, 'Bob': 350}),
-                         (660, "(Alice gets {2} with rent 310, Bob gets {1} with rent 350)"))
+                         (660, {"Alice": {2: 310}, "Bob": {1: 350}}))
 
         # Checking zero values
         ex6 = AgentList({"Alice": {'1': 0, '2': 0}, "Bob": {'1': 0, '2': 0}})
-        self.assertEqual(algorithm1(ex6, 1000, {'Alice': 250, 'Bob': 250}),
-                         "no solution")
+        self.assertEqual(algorithm1(ex6, 1000, {'Alice': 250, 'Bob': 250}), {})
 
         # Checking wrong values
         ex7 = AgentList({"Alice": {'1': 300, '2': 250}, "Bob": {'1': 280, '2': 320}, "Clair": {'1': 400, '2': 370}})
-        self.assertEqual(algorithm1(ex7, 700, {'Alice': 280, 'Bob': 290,'Clair': 380}),
-                         "no solution")
+        self.assertEqual(algorithm1(ex7, 700, {'Alice': 280, 'Bob': 290,'Clair': 380}), {})
 
         # Checking envy case
         ex8 = AgentList({"Alice": {'1': 400, '2': 250}, "Bob": {'1': 400, '2': 320}})
         self.assertEqual(algorithm1(ex8, 700, {'Alice': 420, 'Bob': 410}),
-                         (680, "(Alice gets {1} with rent 400, Bob gets {2} with rent 280)"))
+                         (680, {"Alice": {1: 400}, "Bob": {2: 280}}))
 
     def test_algo2(self):
         ex1 = AgentList({"Alice": {'1': 250, '2': 250, '3': 500}, "Bob": {'1': 250, '2': 250, '3': 500},
                          "Clair": {'1': 250, '2': 500, '3': 250}})
         self.assertEqual(algorithm2(ex1, 1000, {'Alice': 250, 'Bob': 320, 'Clair': 430}),
-                         (920, "(Alice gets {1} with rent 130, Bob gets {2} with rent 270,"
-                               "Clair gets {3} with rent 520)"))
+                         {"Alice": {1: 130}, "Bob": {2: 270}, "Clair": {3: 520}})
 
         ex2 = AgentList({"Alice": {'1': 250, '2': 250, '3': 500}, "Bob": {'1': 250, '2': 250, '3': 500},
                          "Clair": {'1': 250, '2': 250, '3': 500}})
-        self.assertEqual(algorithm2(ex2, 1000, {'Alice': 300, 'Bob': 300, 'Clair': 300}),
-                         "no solution")
+        self.assertEqual(algorithm2(ex2, 1000, {'Alice': 300, 'Bob': 300, 'Clair': 300}), "no solution")
 
         ex3 = AgentList({"Alice": {'1': 250, '2': 750}, "Bob": {'1': 250, '2': 750}})
         self.assertEqual(algorithm2(ex3, 1000, {'Alice': 600, 'Bob': 500}),
-                         (918.75, "(Alice gets {1} with rent 600, Bob gets {2} with rent 318.75)"))
+                         {"Alice": {1: 600}, "Bob": {2: 318.75}})
 
         # Need to continue from here
         ex4 = AgentList({"Alice": {'1': 250, '2': 250, '3': 500}, "Bob": {'1': 250, '2': 250, '3': 500},
