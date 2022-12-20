652dd881c3571c6c7856527cf9ad92bb94361b5f
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2022-12-20 17:07:54 +0200

    version 0 of all functions

--- fairpy/items/proportional_borda_allocations.py
+++ fairpy/items/proportional_borda_allocations.py
@@ -11,34 +11,14 @@ Since:  2022-12
 
 from fairpy import AgentList
 from fairpy.allocations import Allocation
-from typing import List, Any, Dict, Tuple
+from typing import List, Tuple
 import networkx as nx
-import matplotlib.pyplot as plt
-
-
-def draw_graph(G):
-    pos=nx.spring_layout(G)
-    draw_options = {
-        "font_size": 7,
-        "node_size": 1200,
-        "node_color": "yellow",
-        "edgecolors": "black",
-        "linewidths": 1,
-        "width": 1,
-        "with_labels": True,
-        "pos": pos,
-    }
-    nx.draw(G,  **draw_options)
-    labels = nx.get_edge_attributes(G,'weight')
-    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    plt.show()
-
 
 agents_to_test_0 = AgentList({"Shlomo": {"A": 0, "B": 1, "C": 2, "D": 3},"Shira": {"A": 2, "B": 0, "C": 1, "D": 3},"Hadar": {"A": 2, "B": 0, "C": 1, "D": 3},"Or": {"A": 3, "B": 2, "C": 1, "D": 0}})
 def proportional_division_equal_number_of_items_and_players(agents: AgentList) -> Allocation:
     """
     Proposition 2 from "Proportional Borda Allocations":
-    Finds a proportional distribution for the items 
+    Finds a proportional division for the items 
     
     :param agents: represents the n agents.
     :param items: n the items that are allocated.
@@ -72,7 +52,102 @@ def proportional_division_equal_number_of_items_and_players(agents: AgentList) -
     bundles = bundles_from_edges(match, G)
     return Allocation(agents, bundles)
 
+def proportional_division_with_p_even(agents: AgentList) -> Allocation:
+    """
+    Proposition 3 from "Proportional Borda Allocations":
+    Finds a proportional division for the items
+    :param agents: represents the agents.
+    :param items: The items which are being allocated.
+    :return: the proportional allocation, There is always a proportional allocation
+    Notes: 
+        1. p:= len(items)/len(agents) must be an even positive integer
+        2. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
 
+    >>> proportional_division_with_p_even(agents=AgentList([[0,1,2,3],[2,0,1,3]])).map_agent_to_bundle()
+    {'Agent #0': [1, 3], 'Agent #1': [0, 2]}
+    """
+    n = len(agents)
+    k = len(agents.all_items())
+    if k % n != 0:
+        raise ValueError(f"The number of items must be multiple of the number of agents, but they are not: {k}, {n}")
+    if not isBordaCount(agents):
+        raise ValueError(f'Evaluation of items by the agents must be defined by "Board scores". but it is not')
+    p = k/n
+    if p % 2 != 0:
+        raise ValueError(f"The number of items divided by the number of agents must be even but it is not:{k}/{n}={p}")
+    unselected_items = list(agents.all_items())
+    allocation = [[] for i in range(n)]
+    _, allocation = selection_by_order(agents=agents, items=unselected_items ,allocation=allocation, num_iteration=int(p/2))
+    return Allocation(agents, allocation)
+
+agents_to_test_1 = AgentList([[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]])
+def proportional_division_with_number_of_agents_odd(agents: AgentList) -> Allocation:
+    """
+    Theorem 1 from "Proportional Borda Allocations":
+    Finds a proportional division for the items
+    :param agents: represents the agents.
+    :param items: The items which are being allocated.
+    :return: the proportional allocation, There is always a proportional allocation
+    Notes: 
+        1. len(agents) must be an odd positive integer and len(items)/len(agents) must be an positive integer
+        2. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
+    
+    >>> proportional_division_with_number_of_agents_odd(agents=agents_to_test_1).map_agent_to_bundle()
+    {'Agent #0': [0, 4, 8, 9, 14], 'Agent #1': [1, 5, 6, 10, 13], 'Agent #2': [2, 3, 7, 11, 12]}
+    """
+    n = len(agents)
+    if n % 2 != 1:
+        raise ValueError(f"The number of agents must be odd but it is not: {n}")
+    # If n is even, there is a proportional division and the proportional_division function returns it
+    return proportional_division(agents)  
+
+def proportional_division(agents: AgentList) -> Allocation:
+    """
+    Theorem 3 from "Proportional Borda Allocations":
+    Finds a proportional division for the items
+    :param agents: represents the agents.
+    :param items: The items which are being allocated.
+    :return the proportional allocation, 
+        or an approximately proportional allocation if no proportional allocation exists.
+
+    Notes: 
+    1. len(items)/len(agents) must be an positive integer
+    2. approximately proportional allocation If and only if  
+            valuation[i][items of agent[i]]  ≥  |  valuation[i](items)/len(agents) | for each agent i
+                                                |__                              __|
+    3. There is always an approximately proportional allocation
+    4. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
+
+    >>> proportional_division(agents=AgentList([[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]])).map_agent_to_bundle()
+    {'Agent #0': [0, 3, 5, 6, 9], 'Agent #1': [1, 2, 4, 7, 8]}
+    >>> proportional_division(agents= AgentList({"Shlomo": [0,1,2,3],"Dani": [2,0,1,3]})).map_agent_to_bundle()
+    {'Shlomo': [1, 3], 'Dani': [0, 2]}
+
+    """
+    n = len(agents)
+    k = len(agents.all_items())
+    if k % n != 0:
+        raise ValueError(f"The number of items must be multiple of the number of agents, but they are not: {k}, {n}")
+    if k/n % 2 == 0:
+        return proportional_division_with_p_even(agents)
+    if k/n == 1:
+        return proportional_division_equal_number_of_items_and_players(agents)
+    if not isBordaCount(agents):
+        raise ValueError(f'Evaluation of items by the agents must be defined by "Board scores". but it is not')
+    p = k/n - 3                 # 3 ≤ k/n is odd
+    q1 = [i for i in range(n)]
+    q2 = [i-1 for i in range(n, 0, -2)]
+    q3 = [i-1 for i in range(n-1, 0, -2)]
+    order = q1 + q2 + q3 + q3 + q2
+    items = list(agents.all_items())
+    allocation = [[] for i in range(n)]
+    unselected_items, allocation = selection_by_order(agents, items=items, allocation=allocation, order=order)
+    _, allocation = selection_by_order(agents, items=unselected_items, allocation=allocation, num_iteration=int(p/2))
+    return Allocation(agents, allocation)
+
+
+
+#################        Helper function    #################
 def bundles_from_edges(match:set, G:nx.Graph) -> dict:
     bundles = {}
     for edge in match:
@@ -106,117 +181,22 @@ def isBordaCount(agents:AgentList) -> bool:
                 return False
     return True
 
+def selection_by_order(agents:AgentList, items:list, allocation:List[list], num_iteration:int=1, order:list=None) -> Tuple[list,List[list]]:
+    if not order:
+        order = [i for i in range(len(agents))]
+        order += reversed(order)
 
-def proportional_division_with_p_even(agents: AgentList) -> Allocation:
-    """
-    Proposition 3 from "Proportional Borda Allocations":
-    Finds a proportional distribution for the items
-    :param agents: represents the agents.
-    :param items: The items which are being allocated.
-    :return: the proportional allocation, There is always a proportional allocation
-    Notes: 
-        1. p:= len(items)/len(agents) must be an even positive integer
-        2. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
-
-    >>> proportional_division_with_p_even(agents=AgentList([[0,1,2,3],[2,0,1,3]])).map_agent_to_bundle()
-    {'Agent #0': [1, 3], 'Agent #1': [0, 2]}
-    """
-    n = len(agents)
-    k = len(agents.all_items())
-    if k % n != 0:
-        raise ValueError(f"The number of items must be multiple of the number of agents, but they are not: {k}, {n}")
-    if not isBordaCount(agents):
-        raise ValueError(f'Evaluation of items by the agents must be defined by "Board scores". but it is not')
-    p = k/n
-    if p % 2 != 0:
-        raise ValueError(f"The number of items divided by the number of agents must be even but it is not:{k}/{n}={p}")
-    s1 = [i for i in range(n)]
-    s2 = list(s1)
-    s2.reverse()
-    order = s1 + s2
-    unselected_items = list(agents.all_items())
-    allocation = [[] for i in range(n)]
-    for i in range(int(p/2)):
-        unselected_items, allocation = selection_by_order(agents, order, unselected_items ,allocation)
-    return Allocation(agents, allocation)
-
-
-def selection_by_order(agents:AgentList, order:list, items:list, allocation:List[list]) -> Tuple[list,List[list]]:
-    for i in order:
-        agent = agents[i]
-        favorite_index = agent.best_index(items)
-        favorite = items[favorite_index]
-        allocation[i].append(favorite)
-        items.remove(favorite)
+    for iter in range(num_iteration):
+        for i in order:
+            agent = agents[i]
+            favorite_index = agent.best_index(items)
+            favorite = items[favorite_index]
+            allocation[i].append(favorite)
+            items.remove(favorite)
     return items, allocation
 
-agents_to_test_1 = AgentList([[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0],[14,13,12,11,10,9,8,7,6,5,4,3,2,1,0]])
-def proportional_division_with_number_of_agents_odd(agents: AgentList) -> Allocation:
-    """
-    Theorem 1 from "Proportional Borda Allocations":
-    Finds a proportional distribution for the items
-    :param agents: represents the agents.
-    :param items: The items which are being allocated.
-    :return: the proportional allocation, There is always a proportional allocation
-    Notes: 
-        1. len(agents) must be an odd positive integer and len(items)/len(agents) must be an positive integer
-        2. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
-    
-    >>> proportional_division_with_number_of_agents_odd(agents=agents_to_test_1).map_agent_to_bundle()
-    {'Agent #0': [0, 4, 8, 9, 14], 'Agent #1': [1, 5, 6, 10, 13], 'Agent #2': [2, 3, 7, 11, 12]}
-    """
-    n = len(agents)
-    if n % 2 != 1:
-        raise ValueError(f"The number of agents must be odd but it is not: {n}")
-    k = len(agents.all_items())
-    if k % n != 0:
-        raise ValueError(f"The number of items must be multiple of the number of agents, but they are not: {k}, {n}")
-    if k/n % 2 == 0:
-        return proportional_division_with_p_even(agents)
-    if k/n == 1:
-        return proportional_division_equal_number_of_items_and_players(agents)
-    if not isBordaCount(agents):
-        raise ValueError(f'Evaluation of items by the agents must be defined by "Board scores". but it is not')
-    p = k/n - 3                 # 3 ≤ k/n is odd
-    q1 = [i for i in range(n)]
-    q2 = [i-1 for i in range(n, 0, -2)]
-    q3 = [i-1 for i in range(n-1, 0, -2)]
-    q = q1 + q2 + q3 + q3 + q2
-    s = list(q1)
-    q1.reverse()
-    s += q1
-    unselected_items = list(agents.all_items())
-    allocation = [[] for i in range(n)]
-    unselected_items, allocation = selection_by_order(agents, order=q, items=unselected_items, allocation=allocation)
-    for i in range(int(p/2)):
-        unselected_items, allocation = selection_by_order(agents, order=s, items=unselected_items ,allocation=allocation)
-    return Allocation(agents, allocation)
 
 
-def proportional_division(agents: AgentList, items: List[Any]=None) -> Allocation:
-    """
-    Theorem 3 from "Proportional Borda Allocations":
-    Finds a proportional distribution for the items
-    :param agents: represents the agents.
-    :param items: The items which are being allocated.
-    :return the proportional allocation, 
-        or an approximately proportional allocation if no proportional allocation exists.
-
-    Notes: 
-    1. len(items)/len(agents) must be an positive integer
-    2. approximately proportional allocation If and only if  
-            valuation[i][items of agent[i]]  ≥  |  valuation[i](items)/len(agents) | for each agent i
-                                                |__                              __|
-    3. There is always an approximately proportional allocation
-    4. Valuation of agents is defined by "Borda scores" see https://en.wikipedia.org/wiki/Borda_count
-
-    >>> proportional_division(agents=AgentList([[0,1,2,3,4,5,6,7,8,9],[0,1,2,3,4,5,6,7,8,9]]),items=[0,1,2,3,4,5,6,7,8,9]).map_agent_to_bundle()
-    {'Agent #0': [0,3,4,6,9], 'Agent #1': [1,2,5,7,8]}
-    >>> proportional_division(agents= AgentList({"Shlomo": [0,1,2,3],"Dani": [2,0,1,3]}),items=[0,1,2,3]).map_agent_to_bundle()
-    {'Shlomo': [1,3], 'Dani': [0,2]} 
-
-    """
-    return Allocation([''],[['']])
 
 ### MAIN
 
--- fairpy/items/test_proportional_borda_allocations.py
+++ fairpy/items/test_proportional_borda_allocations.py
@@ -11,22 +11,26 @@ Since:  2022-12
 from fairpy.items.proportional_borda_allocations import *
 from fairpy import AgentList
 import pytest
-from itertools import permutations, combinations
+from itertools import permutations
+from typing import Callable
+from itertools import chain
 
 
-size_big = 1001
-big_agents = AgentList([list(range(size_big)) for i in range(size_big)])
-def get_matrix_permutations(n):
+
+
+
+def get_agents_with_permutations_of_valuations(n,k):
     ans = []
-    for i in permutations(range(n)):
-        ans.append(i)
+    for i in permutations(range(k)):
+        ans.append(list(i))
         if len(ans) == n:
             break
-    return ans
+    return AgentList(ans)
 
-def is_proportional(alocation:Allocation, approximately:bool=False):
-    n = len(alocation)
-    value_of_all_items = n*(n-1)/2
+def is_proportional(alocation:Allocation, items, approximately:bool=False):
+    k = len(items)
+    n = alocation.num_of_agents
+    value_of_all_items = k*(k-1)/2
     threshold = value_of_all_items/n
     if approximately:
         threshold = int(threshold)
@@ -35,78 +39,103 @@ def is_proportional(alocation:Allocation, approximately:bool=False):
             return False
     return True
 
-def test_proportional_division_equal_number_of_items_and_players():
-    t1000 = proportional_division_equal_number_of_items_and_players(agents=big_agents) 
-    assert t1000 is None  # Validation is the same for all agents
+def get_agents_without_borda():
+    return AgentList({
+        "Shlomo": {"itemA": 0, "itemB": 0, "itemC": 2}, 
+        "Shira": {"itemA": 0, "itemB": 2, "itemC": 0}, 
+        "Hadar": {"itemA": 2, "itemB": 0, "itemC": 0}
+    }), AgentList({
+        "Shlomo": {"itemA": 256, "itemB": 32, "itemC": 0}, 
+        "Shira": {"itemA": 1, "itemB": 2, "itemC": 0}, 
+        "Hadar": {"itemA": 2, "itemB": 1, "itemC": 0}
+    })
 
-    for i in range(50):
-        agentsI = get_matrix_permutations(i)
-        ansI = proportional_division_equal_number_of_items_and_players(agents=agentsI)
-        assert ansI is None or is_proportional(ansI)
-
-    # if len(agents) != len(items)
+def check_if_it_throws__error_if_without_borda(f:Callable):
+    agents_without_borda1, agents_without_borda2 = get_agents_without_borda()
     with pytest.raises(ValueError):
-        # proportional_division_equal_number_of_items_and_players(agents=big_agents)
-        # proportional_division_equal_number_of_items_and_players(agents=big_agents)
-        proportional_division_equal_number_of_items_and_players(agents=big_agents)
+        f(agents=agents_without_borda1)
+    with pytest.raises(ValueError):
+        f(agents=agents_without_borda2)
+
+def test_proportional_division_equal_number_of_items_and_players():
+    check_if_it_throws__error_if_without_borda(f=proportional_division_equal_number_of_items_and_players)
+    size_big = 100
+    big_agents = AgentList([list(range(size_big)) for i in range(size_big)])
+    tBig = proportional_division_equal_number_of_items_and_players(agents=big_agents) 
+    assert tBig is None  # Validation is the same for all agents
+
+    for i in range(1,50):
+        agentsI = get_agents_with_permutations_of_valuations(i,i)
+        ansI = proportional_division_equal_number_of_items_and_players(agents=agentsI)
+        assert ansI is None or is_proportional(ansI, agentsI.all_items())
+  
+    for i in range(1,50):
+        agentsI = get_agents_with_permutations_of_valuations(i,i+1) # len(agents) != len(items)) 
+        with pytest.raises(ValueError):
+            proportional_division_equal_number_of_items_and_players(agents=agentsI)  
+    
+    agents = AgentList(
+        {
+            "Shlomo": {"itemA": 0, "itemB": 1, "itemC": 2}, 
+            "Shira": {"itemA": 0, "itemB": 2, "itemC": 1}, 
+            "Hadar": {"itemA": 2, "itemB": 0, "itemC": 1}
+        })
+    ans = proportional_division_equal_number_of_items_and_players(agents=agents)
+    assert is_proportional(ans, agents.all_items())
+
 
 def test_proportional_division_with_p_even():
+    check_if_it_throws__error_if_without_borda(f=proportional_division_with_p_even)
+
     size3 = 3
     agents3 = AgentList([list(range(size3)) for i in range(size3)])
-    items9 = list(range(3*size3))
-    items3 = list(range(size3))
-
-    # check if 2<=p even
+    agents9 = AgentList([list(range(3*size3)) for i in range(size3)])
+    
+    # Check for cases where p is odd
     with pytest.raises(ValueError):
-        proportional_division_with_p_even(agents=agents3, items=items3)
-        proportional_division_with_p_even(agents=agents3, items=items9)
+        proportional_division_with_p_even(agents=agents3)  # p == 1
+ 
+    with pytest.raises(ValueError):
+        proportional_division_with_p_even(agents=agents9)  # p == 3
 
-    for i in range(50):
-        agentsI = get_matrix_permutations(i)
+    size_big = 100
+    for i in chain(range(50), [size_big]):
         for j in range(2,9,2):
-            itemsI = list(range(j*i))  # len(items)/i in (2,4,6,8) and is even
-            ansI = proportional_division_with_p_even(agents=agentsI, items=itemsI)
-            assert is_proportional(ansI)
-
-    ans_big = proportional_division_equal_number_of_items_and_players(agents=big_agents, items=list(range(2*size_big)))
-    assert is_proportional(ans_big)
+            agentsI = get_agents_with_permutations_of_valuations(i,j*i)
+            ansI = proportional_division_with_p_even(agents=agentsI)
+            assert is_proportional(ansI, agentsI.all_items())
 
 
-def test_proportional_division_with_number_of_agents_odd():
-    size = 3
-    agents3 = AgentList([list(range(size)) for i in range(size)])
-    items10 = list(range(3*size+1))
-    items4 = list(range(size+1))
+# def test_proportional_division_with_number_of_agents_odd():
+#     size = 3
+#     agents3 = AgentList([list(range(size)) for i in range(size)])
 
-    # if k/n=p not integer resies error
-    with pytest.raises(ValueError):
-        proportional_division_with_number_of_agents_odd(agents=agents3, items=items4)
-        proportional_division_with_number_of_agents_odd(agents=agents3, items=items10)
+#     # if k/n=p not integer resies error
+#     with pytest.raises(ValueError):
+#         proportional_division_with_number_of_agents_odd(agents=agents3)
 
-    for i in range(1,50,2): # i odd
-        agentsI = get_matrix_permutations(i)
-        for j in range(10):
-            itemsI = list(range(j*i))  # len(items)/i is integer
-            ansI = proportional_division_with_number_of_agents_odd(agents=agentsI, items=itemsI)
-            assert is_proportional(ansI)
+#     for i in range(1,50,2): # i odd
+#         agentsI = get_agents_with_permutations_of_valuations(i))
+#         for j in range(10):
+#             itemsI = list(range(j*i))  # len(items)/i is integer
+#             ansI = proportional_division_with_number_of_agents_odd(agents=agentsI)
+#             assert is_proportional(ansI, agentsI.all_items())
 
-    ans_big = proportional_division_with_number_of_agents_odd(agents=big_agents, items=list(range(2*size_big)))
-    assert is_proportional(ans_big)
+#     ans_big = proportional_division_with_number_of_agents_odd(agents=big_agents)
+#     assert is_proportional(ans_big, big_agents.all_items())
 
-def test_proportional_division():
-    size = 4
-    agents4 = AgentList([list(range(size)) for i in range(size)])
-    items13 = list(range(3*size+1))
-    items17 = list(range(5*size+2))
+# def test_proportional_division():
+    # size = 4
+    # agents4 = AgentList([list(range(size)) for i in range(size)])
 
-    # if k/n=p not integer resies error
-    with pytest.raises(ValueError):
-        proportional_division_with_number_of_agents_odd(agents=agents4, items=items13)
-        proportional_division_with_number_of_agents_odd(agents=agents4, items=items17)
+    # # if k/n=p not integer resies error
+    # with pytest.raises(ValueError):
+    #     proportional_division_with_number_of_agents_odd(agents=agents4)
 
-    for n in range(0,50,2): # n even
-        agentsI = get_matrix_permutations(n)
-        for j in range(1,10,2):
-            itemsI = list(range(j*n))  # len(items)/n is integer odd
-            ansI = proportional_division_with_number_of_agents_odd(agents=agentsI, items=itemsI)
-            assert is_proportional(ansI, approximately=True)
+    # for n in range(0,50,2): # n even
+    #     agentsI = get_agents_with_permutations_of_valuations(n))
+    #     for j in range(1,10,2):
+    #         ansI = proportional_division_with_number_of_agents_odd(agents=agentsI)
+    #         assert is_proportional(ansI, agentsI.all_items())
+
+
