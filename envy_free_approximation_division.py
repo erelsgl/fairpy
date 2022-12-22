77f223619737dc9ae4128801f731fb6e5ef9c840
noamya-sh <noamyashany@gmail.com>
2022-12-22 15:30:48 +0200

    algorithm run

--- envy_free_approximation_division.py
+++ envy_free_approximation_division.py
@@ -1,9 +1,51 @@
 from typing import List, Any
 
-from fairpy import AllocationMatrix, Allocation, ValuationMatrix
+import numpy as np
+
+from fairpy import AllocationMatrix, Allocation, ValuationMatrix, MonotoneValuation
 
 
-def envy_free_approximation(a:Allocation) -> List[List[Any]]:
+def swap_columns(matrix, idx_1, idx_2):
+    temp = np.copy(matrix[:, idx_1])
+    matrix[:, idx_1] = matrix[:, idx_2]
+    matrix[:, idx_2] = temp
+
+
+def get_max(arr, payments) -> float:
+    m = max(zip(arr, payments), key=lambda x: x[0] - x[1])
+    return m[0] - m[1]
+
+
+def get_argmax(arr, payments):
+    key = lambda x: x[0] - x[1]
+    return np.argmax([key(x) for x in zip(arr, payments)])
+
+
+def get_second_max(idx, arr, payments) -> float:
+    temp_a = arr[np.arange(len(arr)) != idx]
+    temp_p = payments[np.arange(len(payments)) != idx]
+    m = max(zip(temp_a, temp_p), key=lambda x: x[0] - x[1])
+    return m[0] - m[1]
+
+
+def check_envy(agent_i, matrix, payments: np.array, bundles: list):
+    if matrix[agent_i][agent_i] - payments[agent_i] < get_max(matrix[agent_i], payments):
+        agent_j = get_argmax(matrix[agent_i], payments)
+        u1 = get_max(matrix[agent_i], payments)
+        u2 = get_second_max(agent_j, matrix[agent_i], payments)
+        swap_columns(matrix, agent_i, agent_j)
+        temp_p = payments[agent_i]
+        payments[agent_i] = payments[agent_j] + (u1 - u2)
+        payments[agent_j] = temp_p
+
+        temp = bundles[agent_i]
+        bundles[agent_i] = bundles[agent_j]
+        bundles[agent_j] = temp
+        return True
+    return False
+
+
+def envy_free_approximation(a: Allocation) -> dict:
     """
     "Achieving Envy-freeness and Equitability with Monetary Transfers" by Haris Aziz (2021),
     https://ojs.aaai.org/index.php/AAAI/article/view/16645
@@ -17,18 +59,27 @@ def envy_free_approximation(a:Allocation) -> List[List[Any]]:
     ...      [15,25,22,20]]
     >>> a = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v), bundles=AllocationMatrix(a)))
-    {"bundles":[[3],[1],[0],[2]],"payments":[10,12,5,0]}
+    {'bundles': [[3], [1], [0], [2]], 'payments': [11.0, 12.0, 5.0, 0.0]}
     >>> v2 = [[0,50,0,0],
     ...       [0,40,0,0],
     ...       [0,30,0,0],
     ...       [0,45,0,0]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v2), bundles=AllocationMatrix(a)))
-    {"bundles":[[1],[0],[2],[3]],"payments":[50,0,0,0]}
+    {'bundles': [[1], [0], [2], [3]], 'payments': [50.0, 0.0, 0.0, 0.0]}
     >>> v3 = [[-5,20,10,25],
     ...      [15,-15,-12,-15],
     ...      [-10,12,9,-5],
     ...      [12,20,30,-10]]
     >>> envy_free_approximation(Allocation(agents = ValuationMatrix(v3), bundles=AllocationMatrix(a)))
-    {"bundles":[[3],[0],[1],[2]],"payments":[5,27,3,0]}
+    {'bundles': [[3], [0], [1], [2]], 'payments': [5.0, 27.0, 3.0, 0.0]}
     """
-    pass
+    value_matrix = a.utility_profile_matrix()
+    payments = np.zeros(a.num_of_agents)
+    bundles = [[i] for i in range(a.num_of_agents)]
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(value_matrix)):
+            flag = check_envy(i, value_matrix, payments, bundles) or flag
+
+    return {"bundles": bundles, "payments": payments.tolist()}
