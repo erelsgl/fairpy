d5532ee7d5ea443ff94867e241bbaf72ebca088a
Israel Yacobovich <israel.jacobowi@gmail.com>
2022-12-25 14:50:15 +0200

    greedy algo works
    more doctest
    passes very simple tests
    probloms with cvxpy

--- fairpy/items/israels_MinMakespan_algorithm.py
+++ fairpy/items/israels_MinMakespan_algorithm.py
@@ -10,7 +10,9 @@ import networkx as nx
 # oop
 from abc import ABC, abstractclassmethod
 # data types
-from typing import Callable, Tuple
+from typing import Callable, Tuple, Iterable, Iterator
+# other
+from itertools import product
 
 
 ''' 
@@ -28,7 +30,7 @@ Cornell University, Ithaca, NY, USA
 '''
 
 
-class scedual(ABC):
+class scedual(ABC, Iterable):
 
     ''' custom output class '''
 
@@ -40,68 +42,74 @@ class scedual(ABC):
 
 
     @property
-    def Jobs(self) -> int: return self.costs.objects()
+    def Jobs(self) -> int: return len(self.costs.objects())
 
     @property
-    def Mechines(self) -> int: return self.costs.agents()
-
-    @property
-    def resolution(self) -> float:
-
-        ''' smallest entry '''
-
-        if self.costs is None: raise RuntimeError('data hasn\'nt been initialised yet')
-
-        return min(min(self.costs, key = min))
+    def Mechines(self) -> int: return len(self.costs.agents())
 
     @property
     def shape(self) -> Tuple[int]: return self.Mechines, self.Jobs
 
+    # a mesure of the smallest value that may make a difference in the instance
+    @property
+    def resolution(self) -> float:
+
+        if self.costs is None: raise RuntimeError('data hasn\'nt been initialised yet')
+
+        def accuracy(num: float) -> int: return len(str(float(num)).split('.')[1])
+
+        return 1 / 10 ** max(accuracy(self.costs[mechine, job])  for mechine, job in self)
+
+    # iterator for indexes covering exactly the data structure
+    def __iter__(self) -> Iterator[int]:
+        return iter(product(self.costs.agents(), self.costs.objects()))
 
     # lazy initilization
     def build(self, cost_matrix: ValuationMatrix):
 
-        # different algorithms can use an instance with the same cost matrix
-        # flyweight design
+        # different algorithms can use an instance with the same cost matrix, Flyweight design
         self.costs = cost_matrix
         self.assignments = np.zeros(self.shape).astype(bool)
 
+    # assigning a job to a mechine to process
     def scedual(self, mechine: int, job: int):
-        '''assigning a job to a mechine to process'''
+
         if self.assignments[mechine, job]: raise KeyError('assignment already scedualed')
         self.assignments[mechine, job] = True
 
-    def clear(self):
-        ''' delet all assignments, start over '''
-        self.assignments = np.zeros(self.shape).astype(bool) 
+    # delet all assignments, start over
+    def clear(self): self.assignments = np.zeros(self.shape).astype(bool) 
+
+    # current workload of spesific mechine
+    def loadOf(self, mechine: int) -> float:
+        return sum(self.costs[mechine, job]  for job in self.costs.objects() if self.assignments[mechine, job])
 
 
     @abstractclassmethod
-    def extract_scedual(self): pass
+    def extract_result(self): pass
 
 
-class scedual_makespan(scedual):
+class scedual_assignment(scedual):
 
     '''
     implementation that keeps track of the actuall makespan
 
-    >>> scd = scedual_makespan()
+    >>> scd = scedual_assignment()
     >>> scd.build(ValuationMatrix([[1, 1], [1, 1]]))
     >>> scd.scedual(0, 0)
     >>> scd.scedual(1, 1)
-    >>> print(scd.extract_scedual())
-    {0 : 0, 1 : 1}
+    >>> print(scd.extract_result())
+    {(1, 1), (0, 0)}
     '''
 
-    def extract_scedual(self): 
+    def extract_result(self):
         return { 
-                    mechine : job
-                    for mechine in range(self.Mechines)
-                    for job in range(self.Jobs)
+                    (mechine, job)
+                    for mechine, job in self
                     if self.assignments[mechine, job]
                 }
 
-class scedual_assignment(scedual):
+class scedual_makespan(scedual):
 
     '''
     implementation that keeps track of the assignment
@@ -110,20 +118,20 @@ class scedual_assignment(scedual):
     >>> scd.build(ValuationMatrix([[1, 1], [1, 1]]))
     >>> scd.scedual(0, 0)
     >>> scd.scedual(1, 1)
-    >>> print(scd.extract_scedual())
+    >>> print(scd.extract_result())
     1
     >>> scd.scedual(0, 1)
-    >>> print(scd.extract_scedual())
+    >>> print(scd.extract_result())
     2
     '''
 
-    def extract_scedual(self):
+    def extract_result(self):
 
-        return max(sum((
+        return max(sum(
                             self.costs[mechine, job]
                             for job in range(self.Jobs)
                             if self.assignments[mechine, job]
-                        ))
+                        )
                         for mechine in range(self.Mechines)
                     )
 
@@ -142,19 +150,45 @@ def greedy(output: scedual) -> None:
     Iterate through all jobs, at each iteration:
     assign to the mechine that minimises the current makspan.
 
-
-    >>> print(greedy(np.array([[1, 2, 2], [2, 2, 3],[5, 1, 5]])).extract_scedual())
+    >>> output = scedual_makespan()
+    >>> output.build(ValuationMatrix([[1, 2, 5], [2, 2, 1],[2, 3, 5]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
     3
-    >>> print(greedy(np.array([[1, 4, 2], [2, 2, 3],[1, 4, 2]])).extract_scedual()) 
+    >>> output.build(ValuationMatrix([[1, 2, 1], [4, 2, 4],[2, 3, 2]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
     2
-    >>> print(greedy(np.array([[1, 4, 2, 3], [2, 2, 3, 1],[1, 4, 2, 2]])).extract_scedual())
+    >>> output.build(ValuationMatrix([[1, 2, 1], [4, 2, 4],[2, 3, 2], [3, 1, 2]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
     2
-    >>> print(greedy(np.array([[1, 4, 2, 3], [2, 5, 5, 3],[1, 4, 3, 7], [10, 3, 3, 10]])).extract_scedual())
-    4
+    >>> output.build(ValuationMatrix([[1, 5, 1, 10], [2, 4, 4, 3], [2, 5, 3, 3], [3, 3, 7, 10]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
+    3
+    >>> output = scedual_assignment()
+    >>> output.build(ValuationMatrix([[1, 2, 5], [2, 2, 1],[2, 3, 5]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
+    {(1, 1), (1, 2), (0, 0)}
+    >>> output.build(ValuationMatrix([[1, 2, 1], [4, 2, 4],[2, 3, 2]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
+    {(1, 1), (0, 2), (0, 0)}
+    >>> output.build(ValuationMatrix([[1, 2, 1], [4, 2, 4],[2, 3, 2], [3, 1, 2]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
+    {(3, 1), (0, 2), (0, 0)}
+    >>> output.build(ValuationMatrix([[1, 5, 1, 10], [2, 4, 4, 3], [2, 5, 3, 3], [3, 3, 7, 10]]))
+    >>> greedy(output)
+    >>> print(output.extract_result())
+    {(3, 1), (0, 2), (1, 3), (0, 0)}
     '''
 
     for job in range(output.Jobs):
-        output.scedual(min(range(output.Mechines), key = lambda mechine : output.costs[mechine, job]), job)
+        output.scedual(min(range(output.Mechines), key = lambda mechine : output.costs[mechine, job] + output.loadOf(mechine)), job)
+
 
 def apprx(output: scedual) -> None:
 
@@ -163,23 +197,24 @@ def apprx(output: scedual) -> None:
     using a LP and a spacial rounding algo
 
 
-    >>> print(apprx(np.array([[1, 2], [2, 1]])).extract_scedual())
+    #>>> print(apprx(np.array([[1, 2], [2, 1]])).extract_result())
     1
-    >>> print(apprx(np.array([[1, 2, 3], [2, 1, 3], [3, 2, 1]])).extract_scedual())
+    #>>> print(apprx(np.array([[1, 2, 3], [2, 1, 3], [3, 2, 1]])).extract_result())
     1
-    >>> print(apprx(np.array([[0.5, 1, 0.25], [2, 0.5, 0.8], [0.6, 2, 1]])).extract_scedual() <= 1.2)
+    #>>> print(apprx(np.array([[0.5, 1, 0.25], [2, 0.5, 0.8], [0.6, 2, 1]])).extract_result() <= 1.2)
     True
-    >>> print(apprx(np.array([[[1, 2, 3], [2, 5, 3],[1, 3, 7], [10, 3, 10]]])).extract_scedual() <= 6)
+    #>>> print(apprx(np.array([[[1, 2, 3], [2, 5, 3],[1, 3, 7], [10, 3, 10]]])).extract_result() <= 6)
     True
     '''
 
     # bounds on solution via a greedy solution
 
-    greedy_sol = scedual_makespan(output.costs)
+    greedy_sol = scedual_makespan()
+    greedy_sol.build(output.costs)
     greedy(greedy_sol)
 
-    upper = output.extract_scedual()
-    lower = upper / output.costs.agents()
+    upper = output.extract_result()
+    lower = upper / output.Mechines
 
     del greedy_sol
 
@@ -209,34 +244,35 @@ def LinearProgram(output: scedual, deadlines: np.ndarray, aprrx_bound: float) ->
     constraints = [variables >= np.zeros(output.shape)]
 
     # out of all mechines that can do job j in time <= aprrx_bound
-    # only 1 may be assgned to handle it, for j in all Jobs
+    # only 1 may be assigned to handle it, for j in all Jobs
     constraints += [
-                    sum((
+                    sum(
                             variables[mechine, job]
                             for mechine in range(output.Mechines)
                             if output.costs[mechine, job] <= aprrx_bound
-                        ))
+                        )
                         == 1
                     for job in range(output.Jobs)
                     ]
     # out of all jobs that mechine m can do in time <= aprrx_bound
-    # m can handle at most deadline-m worth of processing time, for m in all Mechines
+    # m can handle at most deadline-m worth of processing time of'em, for m in all Mechines
     constraints += [
-                    sum((
+                    sum(
                             variables[mechine, job]
                             for job in range(output.Jobs)
                             if output.costs[mechine, job] <= aprrx_bound
-                        ))
+                        )
                         <= deadlines[mechine]
                     for mechine in range(output.Mechines)
                     ]
 
-    # minmise maximum workload aka makespan
-    objective = cvx.Minimize(max((sum(
+    # minimise maximum workload aka makespan
+    objective = cvx.Minimize(max(sum(
                                         variables[mechine, job]
-                                        for job in range(output.Jobs))
-                                        for mechine in range(output.Mechines)
-                            )))
+                                        for job in range(output.Jobs)
+                                    )
+                                    for mechine in range(output.Mechines)
+                                ))
 
     prob = cvx.Problem(objective, constraints)
     prob.solve()
@@ -271,26 +307,25 @@ def Round(output: scedual, fractional_sol: cvx.Problem):
 
 
 
-
 def MinMakespan(algo: MinMakespanAlgo, input: ValuationMatrix, output: scedual, **kwargs):
 
     '''
     generic function for the min-makespan problome
 
-    >>> print(MinMakespan(greedy, np.array([[1, 2, 2], [2, 2, 3],[5, 1, 5]], scedual_makespan()))
+    #>>> print(MinMakespan(greedy, np.array([[1, 2, 2], [2, 2, 3],[5, 1, 5]], scedual_makespan()))
     3
-    >>> print(MinMakespan(aprrx, np.array([[1, 2, 3], [2, 1, 3], [3, 2, 1]], scedual_makespan()))
+    #>>> print(MinMakespan(aprrx, np.array([[1, 2, 3], [2, 1, 3], [3, 2, 1]], scedual_makespan()))
     1
     '''
 
     output.build(input)
     algo(output, **kwargs)
-    return output.extract_scedual()
+    return output.extract_result()
 
 
 def RandomTesting(algo: MinMakespanAlgo, output: scedual, iteration: int, **kwargs):
 
-    '''spesefied amount of random tests generator'''
+    ''' spesefied amount of random tests generator '''
 
     mechines = randint(1, 500)
     jobs = randint(1, 500)
@@ -303,4 +338,4 @@ def RandomTesting(algo: MinMakespanAlgo, output: scedual, iteration: int, **kwar
 if __name__ == '__main__':
 
     import doctest
-    doctest.testmod(verbose = True)
+    doctest.testmod(verbose = True)    
