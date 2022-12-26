35537988c25ab27acaf40c6bedd7346c5ee618b8
Yair Raviv <73100906+YairRaviv@users.noreply.github.com>
2022-12-26 17:13:42 +0200

    Update goods_chores.py

--- fairpy/goods_chores.py
+++ fairpy/goods_chores.py
@@ -36,20 +36,81 @@ def  Generalized_Adjusted_Winner_Algorithm(agent_list :AgentList)->List:
 
     """
     pass
-def  Generalized_Moving_knife_Algorithm(agent_list :AgentList)->List:
+def  Generalized_Moving_knife_Algorithm(agent_list :AgentList , prop_values: dict , remain_interval:list , result : dict)->dict:
     """
     "Fair allocation of indivisible goods and chores" by  Ioannis Caragiannis ,
         Ayumi Igarashi, Toby Walsh and Haris Aziz.(2021) , link
         Algorithm 3:  Finding a Connected PROP1 Allocation
         Programmer: Yair Raviv , Rivka Strilitz
         Example 1: Non-Negative Proportional Utilities
-        >>> Generalized_Moving_knife_Algorithm(AgentList({"Agent1":{"1":0,"2":-1,"3":2,"4":1},"Agent2":{"1":1,"2":3,"3":1,"4":-2},"Agent3":{"1":0,"2":2,"3":0,"4":-1}})){"Agent1":["3","4"],"Agent2":["1"],"Agent3":["2"]}
-        Example 2: Positive and Negative Proportional Utilities
-        >>> Generalized_Moving_knife_Algorithm(AgentList({"Agent1":{"1":0,"2":2,"3":0,"4":-4},"Agent2":{"1":1,"2":-2,"3":1,"4":-2},"Agent3":{"1":0,"2":-4,"3":1,"4":1}})){"Agent1":["1","2","3"],"Agent2":[],"Agent3":["4"]}
+        >>> Generalized_Moving_knife_Algorithm(AgentList({"Agent1":{"1":0,"2":-1,"3":2,"4":1},"Agent2":{"1":1,"2":3,"3":1,"4":-2},"Agent3":{"1":0,"2":2,"3":0,"4":-1}}) , {"Agent1":2/3, "Agent2" : 1, "Agent3" : 1/3} , [0,4] , {"Agent1":[] , "Agent2":[] , "Agent3": []})
+        {'Agent1': ['3', '4'], 'Agent2': ['1'], 'Agent3': ['2']}
 
+        Example 2: Positive and Negative Proportional Utilities
+        >>> Generalized_Moving_knife_Algorithm(AgentList({"Agent1":{"1":0,"2":2,"3":0,"4":-4},"Agent2":{"1":1,"2":-2,"3":1,"4":-2},"Agent3":{"1":0,"2":-4,"3":1,"4":1}}),{"Agent1": (-1 * 2 / 3), "Agent2": (-1 * 2 / 3), "Agent3": (-1 * 2 / 3)},[0, 4], {"Agent1": [], "Agent2": [], "Agent3": []})
+        {'Agent1': ['1', '2', '3'], 'Agent2': [], 'Agent3': ['4']}
 
     """
-    pass
+
+    agents_num = len(agent_list)
+    if agents_num <= 0:
+        return {}
+
+    all_items = list(range(remain_interval[0] +1 ,remain_interval[1] +1))
+    N_plus = []
+    for agent in agent_list:
+        # total value for all items
+        evaluation = sum([agent.value(str(x)) for x in all_items])
+        if evaluation > 0:
+            N_plus.append(agent)
+    if len(N_plus) > 0:
+        if len(N_plus) == 1:
+            # allocate all items to the single agent
+            result[N_plus[0].name()] = [str(x) for x in all_items]
+            # sort the result by agent number
+            return dict(sorted(result.items() , key= lambda agent: int(agent[0][5])))
+        sums = {}
+        for agent in N_plus:
+            sums[agent.name()] = 0
+        curr_bundle = []
+        for i in all_items:
+            curr_bundle.append(str(i))
+            # check if there is agent that claim the current bundle at this iteration
+            for agent in N_plus:
+                sums[agent.name()] += agent.value(str(i))
+                if sums[agent.name()] >= prop_values[agent.name()]:
+                    result[agent.name()] = curr_bundle
+                    agent_list.remove(agent)
+                    # recursive call with : updated result , remain interval (items) and without the current agent
+                    return Generalized_Moving_knife_Algorithm(agent_list=agent_list , prop_values=prop_values , remain_interval=[i,all_items[len(all_items) -1]] , result=result)
+    # if there is no agent with positive total value for the items
+    else:
+        if len(agent_list) == 1:
+            # allocate all items to the single agent
+            result[agent_list[0].name()] = [str(x) for x in all_items]
+            # sort the result by agent number
+            return dict(sorted(result.items(), key=lambda agent: int(agent[0][5])))
+        sums = {}
+        for agent in agent_list:
+            sums[agent.name()] = sum([agent.value(str(x)) for x in all_items])
+        curr_bundle = [str(x) for x in all_items]
+        while len(curr_bundle) > 0:
+            for agent in agent_list:
+                if sums[agent.name()] >= (-1 * prop_values[agent.name()]):
+                    result[agent.name()] = curr_bundle
+                    agent_list.remove(agent)
+                    return Generalized_Moving_knife_Algorithm(agent_list=agent_list, prop_values=prop_values,
+                                                              remain_interval=[len(curr_bundle) , all_items[len(all_items) - 1]],
+                                                              result=result)
+                sums[agent.name()] -= agent.value(curr_bundle[len(curr_bundle) -1])
+            curr_bundle.pop()
+    return result
+
+if __name__ == '__main__':
+    import doctest
+
+    (failures, tests) = doctest.testmod(report=True, optionflags=doctest.NORMALIZE_WHITESPACE + doctest.ELLIPSIS)
+    print("{} failures, {} tests".format(failures, tests))
 
 
 
