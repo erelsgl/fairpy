3d3fca9b211f072d4a36b8448358cda036b0e2d0
eitan <eitan76543@gmail.co.il>
2022-12-26 20:35:22 +0200

    finish_algo_1

--- envy_freeness_and_equitability_with_payments.py
+++ envy_freeness_and_equitability_with_payments.py
@@ -1,103 +1,169 @@
 from fairpy import Allocation, MonotoneValuation
 import logging
-def envy_freeness_and_equitability_with_payments(allocation: dict, evaluation: dict):
-    logging.basicConfig("my_log_file.log", level=logging.DEBUG)
-    looger = logging.getLogger()
 
-    # """
-    # "Achieving Envy-freeness and Equitability with Monetary Transfers" by Haris Aziz (2021),
-    # https://ojs.aaai.org/index.php/AAAI/article/view/16645
-    #
-    # Algorithm 1: Creating envy-freeness and equitability division with the help of a payment function.
-    #
-    # Programmers: Noamya Shani, Eitan Shenkolevski.
-    #
-    # >>> agent_dict = {"A":{"x":70},"B":{"x":60},"C":{"x":40},"D":{"x":80},"E":{"x":55}}
-    # >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict, bundles={"A":{"x"}}))
-    # {"bundles":[[],[],[],[0],[]],"payments":[-16,-16,-16,64,-16]}
-    # >>> eq_value = {"x":10,"y":5,"z":15}
-    # >>> agent_dict2 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
-    # >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict2, bundles={"A":{"x"},"B":{"y"},"C":{"z"}}))
-    # {"bundles":[[0],[1],[2],[]],"payments":[2.5,-2.5,7.5,-7.5]}
-    # >>> a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"xw":20,"yz":30,"yw":30,
-    #     # ... "zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"xyzw":50})
-    #     # >>> b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"xw":40,"yz":60,"yw":45,
-    #     # ... "zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"xyzw":95})
-    #     # >>> c = {"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"xw":65,"yz":25,"yw":35,
-    #     # ... "zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"xyzw":90})
-    #     # >>> d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"xw":25,"yz":25,"yw":30,
-    #     # ... "zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"xyzw":50})
-    #     # >>> agent_dict3 = {"A":a,"B":b,"C":c,"D":d}
-    #     # >>> envy_freeness_and_equitability_with_payments(Allocation(agents = agent_dict3, bundles={"A":{"x"},"B":{"y"},"C":{"z"},"D":{"w"}}))
-    #  {"bundles":[[],[0,1,2],[3],[]],"payments":[-27.75,62.25,-6.75,-27.75]}
-    # """
-    #
-    envy2 = True
-    while envy2:
-        envy2 = False
-        for curr_agent in allocation:
-            envy = compare_2_bundles(curr_agent, allocation, evaluation)
-            if envy:
-                envy2 = True
-    sw_ave = calcuSWave(allocation, evaluation)
-    payments = {}
-    for agent in allocation:
-        payments[agent] = get_from_evaliation(allocation, evaluation, agent, "".join(sorted(allocation[agent]))) - sw_ave
-    print("allocation:",allocation, "  payments:", payments)
-    return ("allocation:",allocation, "  payments:", payments)
-
+logging.basicConfig(filename="my_logger_file.log", level=logging.DEBUG)
+logger = logging.getLogger()
 
 def get_value(agent, boundle_of_agent: list, eval_func: dict):
-    curr_bundles = "".join(map(str, sorted(boundle_of_agent)))
-    if curr_bundles:
+    """"
+    A function that returns an agent's evaluation of a particular bundle
+    >>> get_value("a",["y"], eval_1)
+    20
+    >>> get_value("b",["x", "r"], eval_1)
+    79
+    >>> get_value("B",["x"], eval_2)
+    60
+    >>> get_value("A",[], eval_2)
+    0
+    """
+    curr_bundles = "".join(map(str, sorted(boundle_of_agent))) #Convert the list to a sorted string
+    if curr_bundles:  #if the bundle not empty
         return eval_func[agent][curr_bundles]
     return 0
 
-# print(get_value("a",["y"], eval))
-# print(get_value("b",["x", "r"], eval))
 
-
-def compare_2_bundles(agent_a, allo: dict, eval_func: dict):
-    envy = False
-    for agent_b in allo:
+def compare_2_bundles_and_transfer(agent_a, allo: dict, eval_func: dict):
+    """
+    A function that checks if transferring a bundle to a certain agent will increase the SW, and if so transfers
+    >>> compare_2_bundles_and_transfer("a",allocation_1, eval_1)
+    True
+    >>> print(allocation_1["a"])
+    ['y', 'x', 'r']
+    >>> compare_2_bundles_and_transfer("b",allocation_1, eval_1)
+    False
+    >>> compare_2_bundles_and_transfer("B",allocation_2, eval_2)
+    False
+    >>> print(allocation_2["D"])
+    []
+    >>> compare_2_bundles_and_transfer("D",allocation_2, eval_2)
+    True
+    >>> print(allocation_2["D"])
+    ['x']
+    """
+    is_envy = False
+    for agent_b in allo:  #Go through all the agents (except the current agent) and check if transferring their bundle to them increases social welfare
         if agent_b != agent_a:
             if get_value(agent_a, allo[agent_a] + allo[agent_b], eval_func) > get_value(agent_a, allo[agent_a], eval_func) + get_value(agent_b, allo[agent_b], eval_func):
-                allo[agent_a] = allo[agent_a] + allo[agent_b]
-                allo[agent_b] = []
-                envy = True
-    return envy
+                allo[agent_a] = allo[agent_a] + allo[agent_b]  #Making the transfer
+                allo[agent_b] = []  #agent_b lost his bundle
+                is_envy = True  # set is_envy to True because there is jealousy here
+                logger.debug(f"agent {agent_a} took agent {agent_b}'s bundle")
+    return is_envy
 
 
 def calcuSWave(allocation: dict, evaluation: dict):
-    sum_value = 0
+    """
+    A function to calculate the average social welfare of the current allocation
+    >>> calcuSWave(allocation_1, eval_1)
+    49.5
+    >>> calcuSWave(allocation_2, eval_2)
+    14.0
+    >>> calcuSWave(allocation_3, eval_3)
+    7.5
+    >>> calcuSWave(allocation_4, eval_4)
+    20.5
+    """
+    sum_values = 0
     for agent in allocation:
-        if allocation[agent]:
-            sum_value = sum_value + evaluation[agent]["".join(sorted(allocation[agent]))]
-    return sum_value / len(allocation)
-
-def get_from_evaliation(allo: dict ,eval: dict, agent, bundle):
-    if allo[agent] == []:
-        return 0
-    return eval[agent][bundle]
+        if allocation[agent]:  #Go through all the agents and sum up their social welfare in the given allocation
+            sum_values = sum_values + evaluation[agent]["".join(sorted(allocation[agent]))]
+            logger.debug(f"current sum_value after agent {agent}: {sum_values}")
+    logger.info(f"average of SW: {sum_values / len(allocation)}")
+    return sum_values / len(allocation)  #return the average (the received sum divided by the number of agents)
 
 
-eval = {"a": {"x": 40, "y": 20, "r": 30, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "rx": 79, "rxy": 90}}
-allocation = {"a": ["y"], "b": ["x", "r"]}
+def list_sw(allo, eval, pay_list):
+    """
+    A function to check if we have achieved equality
+    >>> list_sw(allocation_2, eval_2, {'A': -16.0, 'B': -16.0, 'C': -16.0, 'D': 64.0, 'E': -16.0})
+    True
+    >>> list_sw(allocation_2, eval_2, {'A': -16.0, 'B': -16.0, 'C': -15.0, 'D': 64.0, 'E': -16.0})
+    False
+    >>> list_sw(allocation_3, eval_3, {'A': -16.0, 'B': -16.0, 'C': -16.0, 'D': 64.0, 'E': -16.0})
+    False
+    >>> list_sw(allocation_3, eval_3, {'A': 2.5, 'B': -2.5, 'C': 7.5, 'D': -7.5})
+    True
+    """
+    sw_list = []
+    for agent in allo:
+        val = get_value(agent, allo[agent], eval) - pay_list[agent]
+        sw_list.append(val)
+    is_equality = all(x == sw_list[0] for x in sw_list)
+    return is_equality
 
-agent_dict = {"A": {"x": 70}, "B": {"x": 60},"C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
-bundles = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
+
+def envy_freeness_and_equitability_with_payments(allocation: dict, evaluation: dict):
+    """
+    "Achieving Envy-freeness and Equitability with Monetary Transfers" by Haris Aziz (2021),
+    https://ojs.aaai.org/index.php/AAAI/article/view/16645
+
+    Algorithm 1: Creating envy-freeness and equitability division with the help of a payment function.
+
+    Programmers: Noamya Shani, Eitan Shenkolevski.
+
+    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "rx": 79, "rxy": 90}}
+    >>> allocation_1 = {"a": ["y"], "b": ["x", "r"]}
+    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_1, allocation = allocation_1)
+    "allocation: {'a': ['y', 'x', 'r'], 'b': []}, payments: {'a': 50.0, 'b': -50.0}"
+    >>> eval_2 = {"A":{"x":70},"B":{"x":60},"C":{"x":40},"D":{"x":80},"E":{"x":55}}
+    >>> allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
+    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_2, allocation = allocation_2)
+    "allocation: {'A': [], 'B': [], 'C': [], 'D': ['x'], 'E': []}, payments: {'A': -16.0, 'B': -16.0, 'C': -16.0, 'D': 64.0, 'E': -16.0}"
+    >>> eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
+    >>> eval_3 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
+    >>> allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
+    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_3, allocation = allocation_3)
+    "allocation: {'A': ['x'], 'B': ['y'], 'C': ['z'], 'D': []}, payments: {'A': 2.5, 'B': -2.5, 'C': 7.5, 'D': -7.5}"
+    >>> a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"wx":20,"yz":30,"yw":30,"zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"wxyz":50}
+    >>> b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"wx":40,"yz":60,"yw":45,"zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"wxyz":95}
+    >>> c = {"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"wx":65,"yz":25,"yw":35,"zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"wxyz":90}
+    >>> d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"wx":25,"yz":25,"yw":30,"zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"wxyz":50}
+    >>> eval_4 = {"A":a,"B":b,"C":c,"D":d}
+    >>> allocation_4={"A":["x"], "B":["y"], "C":["z"], "D":["w"]}
+    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_4, allocation = allocation_4)
+    "allocation: {'A': [], 'B': ['y', 'x', 'z'], 'C': ['w'], 'D': []}, payments: {'A': -27.75, 'B': 62.25, 'C': -6.75, 'D': -27.75}"
+    """
+    still_envy = True
+    while still_envy:  #The algorithm continue to run as long as there is envy
+        still_envy = False
+        for curr_agent in allocation:  #Go through each of the agents to check if they are jealous
+            is_envy = compare_2_bundles_and_transfer(curr_agent, allocation, evaluation)
+            if is_envy:
+                still_envy = True
+    sw_ave = calcuSWave(allocation, evaluation)  #calculate the average social welfare of the current allocation
+    payments = {}
+    for agent in allocation:
+        payments[agent] = get_value(agent, allocation[agent], evaluation) - sw_ave   #Calculation of the payment to each agent (the distance of the evaluation of the current bundle from the average of social welfare)
+    logger.warning(f"check if {list_sw(allocation, evaluation, payments)}")
+    # print("allocation:", allocation, " payments:", payments)
+    return f'allocation: {allocation}, payments: {payments}'
+
+
+eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "rx": 79, "rxy": 90}}
+allocation_1 = {"a": ["y"], "b": ["x", "r"]}
+
+eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
+allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
 
 eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
-agent_dict2 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
-bundles2 = {"A":["x"],"B":["y"],"C":["z"],"D":[]}
+eval_3 = {"A":eq_value, "B":eq_value, "C":eq_value, "D":eq_value}
+allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
 
 a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"wx":20,"yz":30,"yw":30,"zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"wxyz":50}
 b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"wx":40,"yz":60,"yw":45,"zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"wxyz":95}
 c = {"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"wx":65,"yz":25,"yw":35,"zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"wxyz":90}
 d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"wx":25,"yz":25,"yw":30,"zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"wxyz":50}
-agent_dict3 = {"A":a,"B":b,"C":c,"D":d}
-bundles3={"A":["x"],"B":["y"],"C":["z"],"D":["w"]}
+eval_4 = {"A":a, "B":b, "C":c, "D":d}
+allocation_4={"A":["x"], "B":["y"], "C":["z"], "D":["w"]}
 
-envy_freeness_and_equitability_with_payments(bundles, agent_dict)
-envy_freeness_and_equitability_with_payments(bundles2, agent_dict2)
-envy_freeness_and_equitability_with_payments(bundles3 ,agent_dict3)
+# envy_freeness_and_equitability_with_payments(allocation_1, eval_1)
+# envy_freeness_and_equitability_with_payments(allocation_2, eval_2)
+# envy_freeness_and_equitability_with_payments(allocation_3, eval_3)
+# envy_freeness_and_equitability_with_payments(allocation_4, eval_4)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
+
+
+
