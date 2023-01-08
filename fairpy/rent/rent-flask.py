3e6378f5da7e37f3304b376ff98155e9acb08706
Asif Rot <asif..rot12@gmail.com>
2023-01-08 23:40:22 +0200

    algorithm complete

--- fairpy/rent/rent-flask.py
+++ fairpy/rent/rent-flask.py
@@ -1,6 +1,7 @@
 from flask import *
 from flask_cors import CORS
 from fairpy.rent.Algorithms import optimal_envy_free
+from fairpy.agentlist import AgentList
 
 app = Flask(__name__)
 CORS(app)
@@ -10,9 +11,18 @@ CORS(app)
 def handle_submit():
     data = request.get_json()
     agents = data['agents']
-    # do something with the agents data
-    print(agents)
-    return agents
+    rent = data['rent']
+
+    print(agents, rent)
+    values_dict = {agent['name']: {f'Room{index + 1}': int(value) for index, value in enumerate(agent['values'])} for agent
+                   in agents}
+    print("values", values_dict)
+    print("rent", float(rent))
+    budget_dict = {agent['name']: int(agent['budget']) for agent in agents}
+    print(budget_dict)
+    result = optimal_envy_free(AgentList(values_dict), float(rent), budget_dict)
+    print(result)
+    return jsonify(result)
 
 
 if __name__ == '__main__':
--- fairpy/rent/rent-react/src/App.js
+++ fairpy/rent/rent-react/src/App.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 function App() {
   const [numRooms, setNumRooms] = useState('');
   const [agents, setAgents] = useState([]);
+  const [totalRent, setTotalRent] = useState(0);
+  const [error, setError] = useState('');
 
   function handleNumRoomsChange(event) {
     setNumRooms(event.target.value);
@@ -12,9 +14,14 @@ function App() {
         .fill('')
         .map(() => ({
           name: '',
-          budgets: Array(Number(event.target.value)).fill(''),
+          values: Array(Number(event.target.value)).fill(''),
+          budget: 0,
         })),
     );
+  }
+
+  function handleTotalRentChange(event) {
+    setTotalRent(event.target.value);
   }
 
   function handleAgentChange(event, i) {
@@ -31,17 +38,17 @@ function App() {
     );
   }
 
-  function handleBudgetChange(event, i, j) {
+  function handleValueChange(event, i, j) {
     setAgents(
       agents.map((agent, k) => {
         if (i === k) {
           return {
             ...agent,
-            budgets: agent.budgets.map((budget, l) => {
+            values: agent.values.map((value, l) => {
               if (j === l) {
                 return event.target.value;
               }
-              return budget;
+              return value;
             }),
           };
         }
@@ -50,19 +57,45 @@ function App() {
     );
   }
 
-  function isDisabled() {
-    return agents.some((agent) => !agent.name || agent.budgets.some((budget) => !budget));
+  function handleBudgetChange(event, i) {
+    setAgents(
+      agents.map((agent, j) => {
+        if (i === j) {
+          return {
+            ...agent,
+            budget: event.target.value,
+          };
+        }
+        return agent;
+      }),
+    );
   }
 
-async function handleSubmit(event) {
-  event.preventDefault();
-  try {
-    const response = await axios.post('http://localhost:5000/submit', { agents: agents });
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
+  function isDisabled() {
+    return agents.some((agent) => !agent.name || agent.values.some((value) => !value)) || !totalRent;
   }
-}
+
+    async function handleSubmit(event) {
+    event.preventDefault();
+    let errorFound = false;
+    agents.forEach((agent) => {
+      const valuesTotal = agent.values.reduce((acc, value) => acc + parseInt(value), 0);
+      if (valuesTotal !== parseInt(totalRent)) {
+        setError(`Total rent does not match sum of values for ${agent.name}`);
+        errorFound = true;
+      }
+    });
+    if (!errorFound) {
+      setError('');
+      try {
+        console.log("React: ", agents, totalRent)
+        const response = await axios.post('http://localhost:5000/submit', { agents: agents , rent: totalRent});
+        console.log("Flask: ", response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   return (
     <form>
@@ -74,12 +107,18 @@ async function handleSubmit(event) {
           ))}
         </select>
       </label>
+      <br />
+      <label>
+        Total Rent:
+        <input type="text" value={totalRent} onChange={handleTotalRentChange} />
+      </label>
       {numRooms ? (
         <table>
           <thead>
             <tr>
               <th>Agent</th>
               {Array(Number(numRooms)).fill().map((_, i) => <th key={i}>Room {i + 1}</th>)}
+              <th>Budget</th>
             </tr>
           </thead>
           <tbody>
@@ -88,23 +127,27 @@ async function handleSubmit(event) {
                 <td>
                   <input type="text" value={agent.name} onChange={(e) => handleAgentChange(e, i)} />
                 </td>
-                {agent.budgets.map((budget, j) => (
+                {agent.values.map((value, j) => (
                   <td key={j}>
-                    <input type="text" value={budget} onChange={(e) => handleBudgetChange(e, i, j)} />
+                    <input type="text" value={value} onChange={(e) => handleValueChange(e, i, j)} />
                   </td>
                 ))}
+                <td>
+                  <input type="text" value={agent.budget} onChange={(e) => handleBudgetChange(e, i)} />
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       ) : null}
+      <br />
       <button type="submit" onClick={handleSubmit} disabled={isDisabled()}>
-      Submit
+        Submit
       </button>
+      {error && <div>{error}</div>}
     </form>
   );
 }
 
 export default App;
-
 
