bdd5f74e0f639725e2add7184521743781a43831
eitan <eitan76543@gmail.co.il>
2023-01-10 18:10:34 +0200

    set_output

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template
 import pandas as pd
 from fairpy import Allocation, AllocationMatrix, ValuationMatrix
 from envy_free_approximation_division import envy_free_approximation_division
-
+# from gspread_utils import get_worksheet_by_list_of_possible_names
 app = Flask(__name__)
 
 
@@ -42,7 +42,7 @@ def run_algo1(url: str):
     spreadsheet = account.open_by_url(url)
     sheet1 = spreadsheet.worksheet('input')
 
-    sheet1 = spreadsheet.get_worksheet(0)
+    # sheet1 = spreadsheet.get_worksheet(0)
 
     list_of_dicts = sheet1.get_all_records()
     mydict = {}
@@ -66,18 +66,42 @@ def run_algo1(url: str):
     return res
 
 
+def output_sheets_algo1(url: str, result):
+    account = gspread.service_account("credentials.json")
+    spreadsheet = account.open_by_url(url)
+    sheet1 = spreadsheet.worksheet('input')
+    all_agent = sheet1.col_values(1)[1:]
+    size_all_agents = len(all_agent)
+    worksheet_names = [ws.title for ws in spreadsheet.worksheets()]
+    if "output" in worksheet_names:
+        output_sheet = spreadsheet.worksheet("output")
+    else:
+        output_sheet = spreadsheet.add_worksheet(title="output", rows=sheet1.row_count, cols=sheet1.col_count)
+    output_sheet.update_acell("A1", "שם הסוכן")
+    output_sheet.update_acell("B1", "חבילות")
+    output_sheet.update_acell("C1", "תשלום")
+    for i in range(size_all_agents):
+        output_sheet.update_acell(f"A{i + 2}", all_agent[i])
+    all_bundles = list(result["allocation"].values())
+    for i in range(len(all_bundles)):
+        output_sheet.update_acell(f"B{i+2}", "".join(all_bundles[i]))
+    all_payments = list(result["payments"].values())
+    for i in range(len(all_payments)):
+        output_sheet.update_acell(f"C{i + 2}", all_payments[i])
+    return output_sheet.url
+
 @app.route('/play_algo1')
 def run_the_algo1():
     url = request.args.get('url')
     result = run_algo1(url=url)
     print("Run complete")
-    return render_template(f'answer.html', result=result)
+    output = output_sheets_algo1(url, result)
+    return render_template(f'answer.html', url=output, result=result)
 
 
 def run_algo2(url: str, eps: float):
     account = gspread.service_account("credentials.json")
     spreadsheet = account.open_by_url(url)
-
     sheet1 = spreadsheet.get_worksheet(0)
     df = pd.DataFrame(sheet1.get_all_records())
     df = df.set_index(df.columns[0]).to_numpy()
@@ -90,11 +114,36 @@ def run_the_algo2():
     url = request.args.get('url')
     eps = float(request.args.get('eps'))
     result = run_algo2(url, eps)
-    return render_template(f'answer.html', result=result)
+    # output = output_sheets_algo1(url, result)
+    # ---need insert the 'output' to 'url' in next line----
+    return render_template(f'answer.html', url=url, result=result)
+
+
+def get_worksheet_by_list_of_possible_names(spreadsheet:gspread.Spreadsheet, possible_names:list)->gspread.Worksheet:
+	"""
+	Searches the given spreadsheet for a worksheet with a name from the given list.
+	If none is found, return None.
+	"""
+	worksheet_names = [ws.title for ws in spreadsheet.worksheets()]
+	for name in possible_names:
+		if name in worksheet_names:
+			return spreadsheet.worksheet(name)
+	return None
+
+
+
+def worksheet(spreadsheet:gspread.Spreadsheet)->gspread.Worksheet:
+    output_sheet = get_worksheet_by_list_of_possible_names(spreadsheet, "input")
+    print(type(output_sheet))
+    return output_sheet
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    # account = gspread.service_account("credentials.json")
+    # spreadsheet = account.open_by_url()
+    # print(worksheet(spreadsheet))
+    # app.run(debug=True, host="0.0.0.0", port=5200)
 
 # rendering the HTML page which has the button
 # @app.route('/log')
