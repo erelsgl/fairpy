acf9cb5503eeeeeccd8a9e2b3f5a75fed1f8c246
eitan <eitan76543@gmail.co.il>
2023-01-16 15:58:06 +0200

    add_test1@arbitary1

--- fairpy/items/envy_freeness_and_equitability_with_payments.py
+++ fairpy/items/envy_freeness_and_equitability_with_payments.py
@@ -6,31 +6,78 @@
 
     Programmers: Noamya Shani, Eitan Shenkolevski.
 """
-
-from fairpy import Allocation
+import fairpy
+from fairpy import Allocation, AgentList
 import logging
 
 logging.basicConfig(filename="../../my_logger_file.log", level=logging.DEBUG)
 logger = logging.getLogger()
 
 
-def envy_freeness_and_equitability_with_payments(evaluation: dict, allocation: Allocation):
+def find_envy_freeness_and_equitability_with_payments(evaluation: AgentList):
+    """
+        A function that receives values of agents, initializes an initial alloction and sends it to the central function
+
+        :param evaluation: A AgentList of the evaluations of each agent for each bundle
+
+        >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "xy":65, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "xy":55, "rx": 79, "rxy": 90}}
+        >>> find_envy_freeness_and_equitability_with_payments(evaluation=eval_1)
+        {'allocation': {'a': ['r', 'x'], 'b': ['y']}, 'payments': {'a': 25.0, 'b': -25.0}}
+        >>> eval_2 = {"A":{"x":70},"B":{"x":60},"C":{"x":40},"D":{"x":80},"E":{"x":55}}
+        >>> find_envy_freeness_and_equitability_with_payments(evaluation=eval_2)
+        {'allocation': {'A': [], 'B': [], 'C': [], 'D': ['x'], 'E': []}, 'payments': {'A': -16.0, 'B': -16.0, 'C': -16.0, 'D': 64.0, 'E': -16.0}}
+        >>> eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
+        >>> eval_3 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
+        >>> find_envy_freeness_and_equitability_with_payments(evaluation=eval_3)
+        {'allocation': {'A': ['x'], 'B': ['y'], 'C': ['z'], 'D': []}, 'payments': {'A': 2.5, 'B': -2.5, 'C': 7.5, 'D': -7.5}}
+        >>> a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"wx":20,"yz":30,"yw":30,"zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"wxyz":50}
+        >>> b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"wx":40,"yz":60,"yw":45,"zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"wxyz":95}
+        >>> c = {"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"wx":65,"yz":25,"yw":35,"zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"wxyz":90}
+        >>> d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"wx":25,"yz":25,"yw":30,"zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"wxyz":50}
+        >>> eval_4 = {"A":a,"B":b,"C":c,"D":d}
+        >>> find_envy_freeness_and_equitability_with_payments(evaluation = eval_4)
+        {'allocation': {'A': [], 'B': ['y', 'x', 'z'], 'C': ['w'], 'D': []}, 'payments': {'A': -27.75, 'B': 62.25, 'C': -6.75, 'D': -27.75}}
+    """
+    if isinstance(evaluation, AgentList):
+        eval = list(evaluation.all_items())
+        bundles = [bundle for bundle in eval if len(bundle) == 1]
+        agents = evaluation.agent_names()
+    else:
+        eval = list(list(evaluation.values()))[0]
+        bundles = [bundle for bundle in eval if len(bundle) == 1]
+        agents = evaluation.keys()
+    allocation = {}
+    i = 0
+    while i < len(bundles):
+        for agent in evaluation:
+            if i < len(bundles):
+                if agent not in allocation.keys():
+                    allocation[agent] = []
+                if len(allocation[agent]) > 0:
+                    allocation[agent].append(bundles[i])
+                else:
+                    allocation[agent] = [bundles[i]]
+            i = i + 1
+    return make_envy_freeness_and_equitability_with_payments(evaluation, Allocation(agents, allocation))
+
+
+def make_envy_freeness_and_equitability_with_payments(evaluation: AgentList, allocation: Allocation):
     """
     :param evaluation: A dictionary of the evaluations of each agent for each bundle
     :param allocation: The initial allocation
 
-    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "rx": 79, "rxy": 90}}
+    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "xy":65, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "xy":55, "rx": 79, "rxy": 90}}
     >>> allocation_1 = {"a": ["y"], "b": ["x", "r"]}
-    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_1, allocation= Allocation(agents = ["a", "b"],bundles = allocation_1))
+    >>> make_envy_freeness_and_equitability_with_payments(evaluation = eval_1, allocation= Allocation(agents = ["a", "b"],bundles = allocation_1))
     {'allocation': {'a': ['y', 'r', 'x'], 'b': []}, 'payments': {'a': 50.0, 'b': -50.0}}
     >>> eval_2 = {"A":{"x":70},"B":{"x":60},"C":{"x":40},"D":{"x":80},"E":{"x":55}}
     >>> allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
-    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_2, allocation = Allocation(agents = ["A", "B", "C", "D", "E"],bundles = allocation_2))
+    >>> make_envy_freeness_and_equitability_with_payments(evaluation = eval_2, allocation = Allocation(agents = ["A", "B", "C", "D", "E"],bundles = allocation_2))
     {'allocation': {'A': [], 'B': [], 'C': [], 'D': ['x'], 'E': []}, 'payments': {'A': -16.0, 'B': -16.0, 'C': -16.0, 'D': 64.0, 'E': -16.0}}
     >>> eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
     >>> eval_3 = {"A":eq_value,"B":eq_value,"C":eq_value,"D":eq_value}
     >>> allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
-    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_3, allocation = Allocation(agents = ["A", "B", "C", "D"],bundles = allocation_3))
+    >>> make_envy_freeness_and_equitability_with_payments(evaluation = eval_3, allocation = Allocation(agents = ["A", "B", "C", "D"],bundles = allocation_3))
     {'allocation': {'A': ['x'], 'B': ['y'], 'C': ['z'], 'D': []}, 'payments': {'A': 2.5, 'B': -2.5, 'C': 7.5, 'D': -7.5}}
     >>> a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"wx":20,"yz":30,"yw":30,"zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"wxyz":50}
     >>> b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"wx":40,"yz":60,"yw":45,"zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"wxyz":95}
@@ -38,7 +85,7 @@ def envy_freeness_and_equitability_with_payments(evaluation: dict, allocation: A
     >>> d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"wx":25,"yz":25,"yw":30,"zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"wxyz":50}
     >>> eval_4 = {"A":a,"B":b,"C":c,"D":d}
     >>> allocation_4={"A":["x"], "B":["y"], "C":["z"], "D":["w"]}
-    >>> envy_freeness_and_equitability_with_payments(evaluation = eval_4, allocation = Allocation(agents = ["A", "B", "C", "D"],bundles = allocation_4))
+    >>> make_envy_freeness_and_equitability_with_payments(evaluation = eval_4, allocation = Allocation(agents = ["A", "B", "C", "D"],bundles = allocation_4))
     {'allocation': {'A': [], 'B': ['y', 'x', 'z'], 'C': ['w'], 'D': []}, 'payments': {'A': -27.75, 'B': 62.25, 'C': -6.75, 'D': -27.75}}
     """
     if isinstance(allocation, Allocation):
@@ -80,6 +127,8 @@ def get_value(agent: str, boundle: list, eval_func: dict):
     :param boundle: List the elements in the given bundle
     :param eval_func: A dictionary of the evaluations of each agent for each bundle
 
+    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "xy":65, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "xy":55, "rx": 79, "rxy": 90}}
+    >>> eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
     >>> get_value("a",["y"], eval_1)
     20
     >>> get_value("b",["x", "r"], eval_1)
@@ -102,6 +151,10 @@ def compare_2_bundles_and_transfer(agent_a: str, allo: dict, eval_func: dict):
     :param allo: The current allocation
     :param eval_func: A dictionary of the evaluations of each agent for each bundle
 
+    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "xy":65, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "xy":55, "rx": 79, "rxy": 90}}
+    >>> allocation_1 = {"a": ["y"], "b": ["x", "r"]}
+    >>> eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
+    >>> allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
     >>> compare_2_bundles_and_transfer("a",allocation_1, eval_1)
     True
     >>> print(allocation_1["a"])
@@ -134,14 +187,19 @@ def calcuSWave(allocation: dict, eval_func: dict):
     :param allocation: The current allocation
     :param eval_func: A dictionary of the evaluations of each agent for each bundle
 
+    >>> eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "xy":65, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "xy":55, "rx": 79, "rxy": 90}}
+    >>> allocation_1 = {"a": ["y"], "b": ["x", "r"]}
+    >>> eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
+    >>> allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
+    >>> eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
+    >>> eval_3 = {"A":eq_value, "B":eq_value, "C":eq_value, "D":eq_value}
+    >>> allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
     >>> calcuSWave(allocation_1, eval_1)
     49.5
     >>> calcuSWave(allocation_2, eval_2)
     14.0
     >>> calcuSWave(allocation_3, eval_3)
     7.5
-    >>> calcuSWave(allocation_4, eval_4)
-    20.5
     """
     sum_values = 0
     for agent in allocation:
@@ -159,6 +217,11 @@ def check_equal(allo: dict, eval_func: dict, pay_list: dict):
     :param eval_func: A dictionary of the evaluations of each agent for each bundle
     :param pay_list: A dictionary of each agent's payment
 
+    >>> eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
+    >>> allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
+    >>> eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
+    >>> eval_3 = {"A":eq_value, "B":eq_value, "C":eq_value, "D":eq_value}
+    >>> allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
     >>> check_equal(allocation_2, eval_2, {'A': 56.0, 'B': -14.0, 'C': -14.0, 'D': -14.0, 'E': -14.0})
     True
     >>> check_equal(allocation_2, eval_2, {'A': -16.0, 'B': -16.0, 'C': -15.0, 'D': 64.0, 'E': -16.0})
@@ -176,27 +239,10 @@ def check_equal(allo: dict, eval_func: dict, pay_list: dict):
     return is_equality
 
 
-eval_1 = {"a": {"x": 40, "y": 20, "r": 30, "rx": 80, "rxy": 100}, "b": {"x": 10, "y":30, "r": 70, "rx": 79, "rxy": 90}}
-allocation_1 = {"a": ["y"], "b": ["x", "r"]}
-
-eval_2 = {"A": {"x": 70}, "B": {"x": 60}, "C": {"x": 40}, "D": {"x": 80}, "E": {"x": 55}}
-allocation_2 = {"A": ["x"], "B": [], "C": [], "D": [], "E": []}
-
-eq_value = {"x":10,"y":5,"z":15, "xy": 15, "yz": 20, "xz": 25}
-eval_3 = {"A":eq_value, "B":eq_value, "C":eq_value, "D":eq_value}
-allocation_3 = {"A":["x"], "B":["y"], "C":["z"], "D":[]}
-
-a = {"x": 15, "y": 20, "z":10,"w":5, "xy": 45,"xz":25,"wx":20,"yz":30,"yw":30,"zw":20,"xyz":50,"xyw":50,"xzw":30,"yzw":40,"wxyz":50}
-b = {"x": 30, "y": 35, "z":22,"w":7, "xy": 65,"xz":55,"wx":40,"yz":60,"yw":45,"zw":30,"xyz":90,"xyw":75,"xzw":65,"yzw":65,"wxyz":95}
-c = {"x": 40, "y": 12, "z":13,"w":21, "xy": 55,"xz":55,"wx":65,"yz":25,"yw":35,"zw":35,"xyz":65,"xyw":75,"xzw":75,"yzw":50,"wxyz":90}
-d = {"x": 5, "y": 7, "z":17,"w":19, "xy": 12,"xz":25,"wx":25,"yz":25,"yw":30,"zw":36,"xyz":30,"xyw":35,"xzw":45,"yzw":45,"wxyz":50}
-eval_4 = {"A":a, "B":b, "C":c, "D":d}
-allocation_4={"A":["x"], "B":["y"], "C":["z"], "D":["w"]}
-
-
 if __name__ == '__main__':
     import doctest
     print(doctest.testmod())
+
 
 
 
