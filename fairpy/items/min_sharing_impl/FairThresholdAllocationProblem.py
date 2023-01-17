bf3a4f54395c54188b92522fe2e8dcd0758ec801
Erel Segal-Halevi <erelsgl@gmail.com>
2023-01-17 12:01:12 +0200

    round to multiple

--- fairpy/items/min_sharing_impl/FairThresholdAllocationProblem.py
+++ fairpy/items/min_sharing_impl/FairThresholdAllocationProblem.py
@@ -78,13 +78,13 @@ class FairThresholdAllocationProblem(FairAllocationProblem):
         None
         >>> g1 = [[0.0, 0.0, 0.0, 1], [0.0, 1, 1, 1], [1, 1, 0.0, 1]]
         >>> g = ConsumptionGraph(g1)
-        >>> print(fpap.find_allocation_for_graph(g).round(1))
-        [[0.  0.  0.  0.9]
-         [0.  0.6 1.  0. ]
-         [1.  0.4 0.  0. ]]
+        >>> print(fpap.find_allocation_for_graph(g).round_to_multiple_of(0.2))
+        [[0.  0.  0.  0.8]
+         [0.  0.4 1.  0. ]
+         [1.  0.6 0.  0. ]]
         >>> g1 = [[0.0, 0.0, 0.0, 1], [0.0, 0.0, 1, 1], [1, 1, 0.0, 1]]
         >>> g = ConsumptionGraph(g1)
-        >>> fpap.find_allocation_for_graph(g).round(2).num_of_sharings()
+        >>> fpap.find_allocation_for_graph(g).round(1).num_of_sharings()
         1
         >>> g1 = [[0.0, 0.0, 0.0, 1], [0.0, 0.0, 1, 1], [1, 1, 1, 1]]
         >>> g = ConsumptionGraph(g1)
@@ -98,10 +98,10 @@ class FairThresholdAllocationProblem(FairAllocationProblem):
         None
         >>> g1 = [[0.0, 0.0, 0.0, 1], [0.0, 1, 1, 1], [1, 1, 0.0, 0.0]]
         >>> g = ConsumptionGraph(g1)
-        >>> print(fpap.find_allocation_for_graph(g).round(1))
-        [[0.  0.  0.  0.9]
-         [0.  0.6 1.  0.1]
-         [1.  0.4 0.  0. ]]
+        >>> print(fpap.find_allocation_for_graph(g).round_to_multiple_of(0.2))
+        [[0.  0.  0.  0.8]
+         [0.  0.4 1.  0.2]
+         [1.  0.6 0.  0. ]]
 
         # This example exposed a bug in OSQP solver!
         >>> v = ValuationMatrix([ [465,0,535] , [0,0,1000]  ]) 
@@ -109,8 +109,8 @@ class FairThresholdAllocationProblem(FairAllocationProblem):
         >>> g1 = [[1,1,1],[0,0,1]]
         >>> g = ConsumptionGraph(g1)
         >>> print(fpap.find_allocation_for_graph(g).round(1))
-        [[1.  1.  0.6]
-         [0.  0.  0.4]]
+        [[1.  1.  0.4]
+         [0.  0.  0.6]]
         """
         mat = cvxpy.Variable((self.valuation.num_of_agents, self.valuation.num_of_objects))
         constraints = []
